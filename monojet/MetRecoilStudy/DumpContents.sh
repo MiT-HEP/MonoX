485a73cacbb5ef30bdd5c960633093876b939046
Daniel Abercrombie <dabercro@mit.edu>
2015-11-05 17:20:48 +0100

    Added interactive slimmer.

---
+++ monojet/MetRecoilStudy/DumpContents.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+inDir=/afs/cern.ch/work/d/dabercro/eos/cms/store/caf/user/yiiyama/nerov3redo/
+outDir=/afs/cern.ch/work/d/dabercro/public/Winter15/exampleOut
+filesPerTerminal=50
+
+######
+
+if [ ! -d $outDir ]; then
+    mkdir $outDir
+fi
+
+if [ ! -d $outDir/merged ]; then
+    mkdir $outDir/merged
+fi
+
+countFiles=0
+countLists=0
+
+rm TerminalList_*
+
+touch TerminalList_$countLists.txt
+touch $outDir/myHadd.txt
+
+for aDir in `ls $inDir`; do
+    echo $outDir/merged/$aDir.root $outDir/$aDir\_*.root >> $outDir/myHadd.txt
+    for inFile in `ls $inDir/$aDir`; do
+        extension="${inFile##*_}"
+        if [ "${inFile##*_}" = "pilot.root" ]; then
+            echo $inFile
+            continue
+        elif [ "${inFile##*.}" == "root" ]; then
+            echo $inDir/$aDir/$inFile $outDir/$aDir\_$countFiles.root >> TerminalList_$countLists.txt
+            countFiles=$((countFiles + 1))
+            if [ "$countFiles" = "$filesPerTerminal" ]; then
+                countFiles=0
+                countLists=$((countLists + 1))
+                touch TerminalList_$countLists.txt
+            fi
+        fi
+    done
+done
+
--- monojet/MetRecoilStudy/runSlimmer.py
+++ monojet/MetRecoilStudy/runSlimmer.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import sys
+import sys,os
 import ROOT
 
 ROOT.gROOT.LoadMacro('NeroTree.C+')
@@ -28,5 +28,9 @@ elif sys.argv[1] == "TT":
         "root://eoscms//store/user/yiiyama/transfer/TTbarDMJets_pseudoscalar_Mchi-1_Mphi-100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8+RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1+AODSIM/nero_0000.root",
         "testTT.root")
 else:
-    ROOT.NeroSlimmer(sys.argv[1],
-                     sys.argv[2])
+    if not os.path.isfile(sys.argv[2]):
+        ROOT.NeroSlimmer(sys.argv[1],
+                         sys.argv[2])
+    else:
+        print sys.argv[2] + " already exists!! Skipping..."
+    
