0f9de5818ce53be260bf3a7b35edc7d257fef275
Daniel Abercrombie <dabercro@mit.edu>
2015-11-05 17:47:19 +0100

    Fixed bug that might overwrite.

--- monojet/MetRecoilStudy/DumpContents.sh
+++ monojet/MetRecoilStudy/DumpContents.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-inDir=/afs/cern.ch/work/d/dabercro/eos/cms/store/caf/user/yiiyama/nerov3redo/
+inDir=/afs/cern.ch/work/d/dabercro/eos/cms/store/caf/user/yiiyama/nerov3redo
 outDir=/afs/cern.ch/work/d/dabercro/public/Winter15/exampleOut
 filesPerTerminal=50
 
@@ -23,6 +23,7 @@ touch TerminalList_$countLists.txt
 touch $outDir/myHadd.txt
 
 for aDir in `ls $inDir`; do
+    indexFiles=0
     echo $outDir/merged/$aDir.root $outDir/$aDir\_*.root >> $outDir/myHadd.txt
     for inFile in `ls $inDir/$aDir`; do
         extension="${inFile##*_}"
@@ -30,7 +31,8 @@ for aDir in `ls $inDir`; do
             echo $inFile
             continue
         elif [ "${inFile##*.}" == "root" ]; then
-            echo $inDir/$aDir/$inFile $outDir/$aDir\_$countFiles.root >> TerminalList_$countLists.txt
+            echo $inDir/$aDir/$inFile $outDir/$aDir\_$indexFiles.root >> TerminalList_$countLists.txt
+            indexFiles=$((indexFiles + 1))
             countFiles=$((countFiles + 1))
             if [ "$countFiles" = "$filesPerTerminal" ]; then
                 countFiles=0
