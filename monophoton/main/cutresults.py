d356ae40109c67975bade9048fd8d75c2d952d75
Yutaro Iiyama <yiiyama@mit.edu>
2016-02-20 11:26:52 -0500

    event-by-event cut results

---
+++ monophoton/main/cutresults.py
@@ -0,0 +1,43 @@
+import sys
+import array
+import ROOT
+
+fname, event = sys.argv[1:3]
+
+run, lumi, event = event.split(':')
+
+source = ROOT.TFile.Open(fname)
+tree = source.Get('cutflow')
+
+bits = array.array('I', [0])
+tree.SetBranchAddress('cutBits', bits)
+branch = tree.GetBranch('cutBits')
+
+tree.Draw('>>elist', 'run == %s && lumi == %s && event == %s' % (run, lumi, event), 'entrylist')
+elist = ROOT.gDirectory.Get('elist')
+
+tree.SetEntryList(elist)
+iEntry = tree.GetEntryNumber(0)
+
+branch.GetEntry(iEntry)
+
+bitmasks = [
+    'passTrigger',
+    'beginEvent',
+    'vetoMuons',
+    'vetoElectrons',
+    'vetoTaus',
+    'selectPhotons',
+    'cleanJets',
+    'selectMet',
+    'highMet',
+    'metIso',
+]
+
+cutres = bits[0]
+for iB in range(len(bitmasks)):
+    if cutres & (1 << iB) != 0:
+        print bitmasks[iB]
+    else:
+        print '!' + bitmasks[iB]
+
