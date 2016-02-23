974728cac40e3b08419fe98b1bd395aca3fd2113
Daniel Abercrombie <dabercro@mit.edu>
2016-02-23 19:27:41 +0100

    Updated config to run LimitTrees with more central option.

--- monojet/analysis/CrombieAnalysisConfig.sh
+++ monojet/analysis/CrombieAnalysisConfig.sh
@@ -1,5 +1,7 @@
 export CrombieMCConfig=MCConfig.txt
 export CrombieSigConfig=SignalConfig.txt
+export CrombieExcept_gjets=MCGJets.txt
+export CrombieExcept_tt=MCTop.txt
 export CrombieLuminosity=2245.0
 export CrombieInFilesDir=/afs/cern.ch/work/d/dabercro/public/Winter15/SkimOut_160212
 export CrombieOutPlotDir=/afs/cern.ch/user/d/dabercro/www/plots/$CROMBIEDATE
--- monojet/analysis/MakeMonoJetIncLimitTree.py
+++ monojet/analysis/MakeMonoJetIncLimitTree.py
@@ -14,25 +14,7 @@ ltm.SetAllHistName('htotal')
 ltm.SetOutputWeightBranch('scaleMC_w')
 ltm.SetReportFrequency(20)
 
-directory = os.environ['CrombieInFilesDir']
-if not directory.endswith('/'):
-    directory = directory + '/'
-
-ltm.ExceptionSkip('gjets','QCD_100To200')
-ltm.ExceptionSkip('gjets','QCD_200To300')
-ltm.ExceptionSkip('gjets','QCD_300To500')
-ltm.ExceptionSkip('gjets','QCD_500To700')
-ltm.ExceptionSkip('gjets','QCD_700To1000')
-ltm.ExceptionSkip('gjets','QCD_1000To1500')
-ltm.ExceptionAdd('gjets',directory + 'Purity/monojet_GJets_HT-40To100.root','QCD_40To100',23080.0)
-ltm.ExceptionAdd('gjets',directory + 'Purity/monojet_GJets_HT-100To200.root','QCD_100To200',9235.0)
-ltm.ExceptionAdd('gjets',directory + 'Purity/monojet_GJets_HT-200To400.root','QCD_200To400',2298.0)
-ltm.ExceptionAdd('gjets',directory + 'Purity/monojet_GJets_HT-400To600.root','QCD_400To600',277.6)
-ltm.ExceptionAdd('gjets',directory + 'Purity/monojet_GJets_HT-600ToInf.root','QCD_600ToInf',93.47)
-
 SetupFromEnv(ltm)
-ltm.ReadMCConfig(os.environ['CrombieMCConfig'],ltm.kBackground)
-ltm.ReadMCConfig(os.environ['CrombieSigConfig'],ltm.kSignal)
 
 if __name__ == '__main__':
     ltm.SetOutFileName('MonoJetIncLimitsTrees.root')
