818f224f89e2b9fa41b4fb2475ad318b18916bfa
dabercro <dabercro@mit.edu>
2016-03-16 19:51:41 +0100

    ROC Curve working.

--- monojet/analysis/vbfROCs.py
+++ monojet/analysis/vbfROCs.py
@@ -1,26 +1,23 @@
 #! /usr/bin/python
 
-from ROOT import TFile
-from CrombieTools.PlotTools.PlotROC import plotter
-import cuts
+from vbfDists import *
 
-inDir = '/afs/cern.ch/work/d/dabercro/public/Winter15/SkimOut_VBFStudy/files/'
-outDir = '/afs/cern.ch/user/d/dabercro/www/VBFStudy/'
+from CrombieTools.PlotTools.PlotROC import plotter
 
 vbfFile = TFile(inDir + 'monojet_VBFSignal.root')
 bkgdFile = TFile(inDir + 'monojet_Bkgd.root')
 
-#plotter.AddVar('mjj')
+plotter.AddVar('mjj')
 plotter.AddVar('jjDEta')
 
-#plotter.AddLegendEntry('Di-jet Mass',1)
-#plotter.AddLegendEntry('Di-jet #Delta#eta',2)
+plotter.AddLegendEntry('Di-jet Mass',1)
+plotter.AddLegendEntry('Di-jet #Delta#eta',2)
 
-#plotter.SetLumiLabel('2.30')
-#plotter.SetIsCMSPrelim(True)
-#plotter.SetLegendLocation(plotter.kLower,plotter.kLeft)
+plotter.SetLumiLabel('2.30')
+plotter.SetIsCMSPrelim(True)
+plotter.SetLegendLocation(plotter.kUpper,plotter.kLeft)
 
-theCut = cuts.cut('monoJet_inc','signal')
+theCut = cuts.cut('monoJet_inc','signal') + '* (jjDEta > 0 && mjj > 0)'
 
 plotter.SetSignalCut(theCut)
 plotter.SetBackgroundCut(theCut)
@@ -29,3 +26,14 @@ plotter.SetSignalTree(vbfFile.events)
 plotter.SetBackgroundTree(bkgdFile.events)
 
 plotter.MakeCanvas(outDir + 'ROC_VBF')
+
+plotter.SetPlotType(plotter.kSignificance)
+plotter.SetLegendLocation(plotter.kUpper,plotter.kRight)
+
+plotter.ResetVars()
+plotter.AddVar('mjj')
+plotter.MakeCanvas(outDir + 'Sig_VBF_mjj',500,'dijet mass cut [GeV]','Significance')
+
+plotter.ResetVars()
+plotter.AddVar('jjDEta')
+plotter.MakeCanvas(outDir + 'Sig_VBF_jjDEta',500,'#Delta#eta cut','Significance')
