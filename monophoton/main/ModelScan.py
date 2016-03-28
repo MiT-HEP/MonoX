e85de511964d8284742cd3d82b03e0e8661f1c8c
Brandon <ballen@mit.edu>
2016-03-28 18:42:47 -0400

    Added functionality to ModelScan.py to spit out nice limit tables. Labels are only correct for DM models right now, need to support ADD as well.

--- monophoton/main/ModelScan.py
+++ monophoton/main/ModelScan.py
@@ -13,9 +13,10 @@ basedir = os.path.dirname(thisdir)
 sys.path.append(basedir)
 from plotstyle import *
 from datasets import allsamples
+import config
 
 parser = ArgumentParser()
-parser.add_argument('-m', '--models', metavar = 'MODEL', action = 'store', default = [], nargs='+', help = 'Signal model(s) to compute limits for.')
+parser.add_argument('-m', '--models', metavar = 'MODEL', action = 'store', default = [], nargs='+', help = 'Class of signal models: add, dmv, dma, dmewk')
 parser.add_argument('-R', '--root-file', metavar = 'PATH', action = 'store', dest = 'rootFile', help = 'Histogram ROOT file.')
 parser.add_argument('--variable', '-v', metavar = 'VARNAME', action = 'store', dest = 'variable', default = 'phoPtHighMet', help = 'Discriminating variable.')
 parser.add_argument('--shape', '-s', action = 'store_true', dest = 'shape', default = False, help = 'Turn on shape analysis.')
@@ -88,10 +89,27 @@ for model in sorted(allsamples):
 
 # pprint(classes)
 
-modelList = opts.models
-if modelList == []:
+modelList = []
+if opts.models == []:
     for sample in allsamples:
         if sample.signal:
+            modelList.append(sample.name)
+
+if 'add' in opts.models:
+    for sample in allsamples:
+        if sample.signal and 'add' in sample.name:
+            modelList.append(sample.name)
+if 'dmv' in opts.models:
+    for sample in allsamples:
+        if sample.signal and 'dmv' in sample.name and not 'fs' in sample.name:
+            modelList.append(sample.name)
+if 'dma' in opts.models:
+    for sample in allsamples:
+        if sample.signal and 'dma' in sample.name and not 'fs' in sample.name:
+            modelList.append(sample.name)
+if 'dmewk' in opts.models:
+    for sample in allsamples:
+        if sample.signal and 'dmewk' in sample.name:
             modelList.append(sample.name)
 
 LimitToolDir = os.path.join(os.environ['CMSSW_BASE'], 'src/HiggsAnalysis/CombinedLimit')
@@ -125,6 +143,7 @@ for iM, model in enumerate(modelList):
     # print err, '\n'
         
     (obs, exp) = RunHiggsTool(cardPath,LimitToolDir)
+    # (obs, exp) = (1.0, 1.0)
 
     if allsamples[model].scale != 1.:
         obs = obs * allsamples[model].scale
@@ -148,5 +167,91 @@ for iM, model in enumerate(modelList):
 
     print limitString
 
-
 print datetime.datetime.now()
+
+# "dmv" : ( [mMed], [mDM] ) 
+for name in opts.models:
+    params = classes[name]
+    xvals = sorted([int(param) for param in params[0]])
+    yvals = sorted([int(param) for param in params[1]])
+
+    fileName = "ModelScan_"+name+".tex"
+    limitDir = os.path.join(config.webDir, "limits")
+    filePath = os.path.join(limitDir, fileName)
+    limitsFile = open(filePath, "w")
+    
+    limitsFile.write(r"\documentclass{article}")
+    limitsFile.write("\n")
+    limitsFile.write(r"\usepackage[paperheight=2in, paperwidth=16in]{geometry}")
+    limitsFile.write("\n")
+    limitsFile.write(r"\begin{document}")
+    limitsFile.write("\n")
+    limitsFile.write(r"\pagenumbering{gobble}")
+    limitsFile.write("\n")
+
+
+    tableString = r"\begin{tabular}{ |r|"
+    ylabelString = ""
+    for xval in xvals:
+        tableString += r"c "
+        ylabelString += r" & "+str(xval)
+    tableString += r"| }"
+    ylabelString += r" \\ "
+
+    limitsFile.write(tableString)
+    limitsFile.write("\n")
+    limitsFile.write(r"\hline")
+    limitsFile.write("\n")
+
+    limitsFile.write(r"$m_\mathrm{DM}$ (GeV) & \multicolumn{"+str(len(params[0]))+r"}{|c|}{$m_\mathrm{MED}$ (GeV)} \\")
+    limitsFile.write("\n")
+    limitsFile.write(r"\hline")
+    limitsFile.write("\n")
+    
+    limitsFile.write(ylabelString)
+    limitsFile.write("\n")
+    limitsFile.write(r"\hline")
+    limitsFile.write("\n")
+    
+    rowStrings = []
+    for yval in yvals:
+        rowString = str(yval)
+        for xval in xvals:
+            model = name+"-"+str(xval)+"-"+str(yval)
+            if model in limits.keys():
+                (obs, exp, obsXsec, expXsec) = limits[model]
+                if obs < 0. or exp < 0.:
+                    rowString += " & "    
+                elif obs > 999.9 or exp > 999.9:
+                    rowString += " & $>1000$ "
+                elif obs < 0.1 or exp < 0.1:
+                    rowString += " & %.1E (%.1E)" % (obs, exp)
+                else:
+                    rowString += " & %.2f (%.2f)" % (obs, exp)
+            else:
+                rowString += " & "
+        rowString += r" \\ "
+        limitsFile.write(rowString)
+        limitsFile.write("\n")
+        
+    limitsFile.write(r"\hline")
+    limitsFile.write("\n")
+
+    limitsFile.write(r"\end{tabular}")
+    limitsFile.write("\n")
+    limitsFile.write(r"\end{document}")
+    limitsFile.write("\n")
+    limitsFile.close()
+
+    pdflatex = Popen( ["pdflatex",filePath,"-interaction nonstopmode"]
+                      ,stdout=PIPE,stderr=PIPE,cwd=limitDir)
+    pdfout = pdflatex.communicate()
+    if not pdfout[1] == "":
+        print pdfout[1]
+            
+    convert = Popen( ["convert",filePath.replace(".tex",".pdf")
+                      ,filePath.replace(".tex",".png") ]
+                     ,stdout=PIPE,stderr=PIPE,cwd=limitDir)
+    conout = convert.communicate()
+    if not conout[1] == "":
+        print conout[1]
