d6dbcd5ef176ef86608099a884e074b593a304c1
Brandon <ballen@mit.edu>
2016-04-19 07:55:32 -0400

    Plotting some stuff for diphoton

--- monophoton/phoMet/plot.py
+++ monophoton/phoMet/plot.py
@@ -7,7 +7,7 @@ import array
 thisdir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(thisdir)
 sys.path.append(basedir)
-from plotstyle import RatioCanvas, DataMCCanvas
+from plotstyle import DataMCCanvas
 from datasets import allsamples
 
 import ROOT as r
@@ -15,11 +15,6 @@ r.gROOT.SetBatch(True)
 
 dataTree = r.TChain('skim')
 dataTree.Add('/scratch5/ballen/hist/monophoton/phoMet/skim_sel-*.root')
-
-dyTree = r.TChain('skim')
-dyTree.Add('/scratch5/ballen/hist/monophoton/phoMet/skim_dy-50-*.root')
-wlnuTree = r.TChain('skim')
-wlnuTree.Add('/scratch5/ballen/hist/monophoton/phoMet/skim_wlnu-*.root')
 
 samples = [ ('zllg', r.TColor.GetColor(0xff, 0x99, 0x33)), 
             ('ttg', r.TColor.GetColor(0xbb, 0xaa, 0xff)), 
@@ -38,12 +33,7 @@ for sample, color in samples:
 lumi = allsamples['sel-d3'].lumi + allsamples['sel-d4'].lumi
 canvas = DataMCCanvas(lumi = lumi)
 
-# canvas = RatioCanvas(lumi = lumi)
-# canvas.legend.setPosition(0.6, 0.7, 0.9, 0.9)
-# canvas.legend.add('data', title = 'Data', lcolor = r.kBlack, lwidth = 2)
-# canvas.legend.add('mc', title = 'MC', mcolor = r.kRed, lcolor = r.kRed, lwidth = 2)
-
-varStrings = '(probe.ptReco / probe.ptCorr)'
+varString = '(probe.ptReco / probe.ptCorr)'
 
 probeEtaCuts = [ ('TMath::Abs(probe.eta) < 0.8', 'low'), ('TMath::Abs(probe.eta > 0.8)', 'high') ] 
 recoilCuts = [ ('recoil.pt > %i.' % pt, 'recoil'+str(pt)) for pt in [170, 200, 250, 300, 400] ]
@@ -72,9 +62,6 @@ for recoilCut in recoilCuts:
         dataTree.Draw(varString+'>>'+dataName, 'weight * ('+cutString+')', 'goff')
         canvas.addObs(dataHist, title = 'Data')
 
-        # canvas.legend.apply('data', dataHist)
-        # canvas.addHistogram(dataHist, drawOpt = 'L')
-
         for sample, color, mcTree in mcTrees:
             mcName = sample+'_'+label
             mcHist = r.TH1D(mcName, '', 20, 0., 2.)
@@ -82,9 +69,6 @@ for recoilCut in recoilCuts:
             mcTree.Draw(varString+'>>'+mcName, str(lumi)+' * weight * ('+cutString+')', 'goff')
 
             canvas.addStacked(mcHist, title = sample, color = color)
-
-            # canvas.legend.apply('mc', mcHist)        
-            # canvas.addHistogram(mcHist, drawOpt = 'L')
 
         canvas.ylimits = (0.001, -1.)
 
