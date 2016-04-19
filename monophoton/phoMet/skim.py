6f75ca741b721d257cef4389bbe3fd25e2549fe1
Brandon <ballen@mit.edu>
2016-04-19 06:45:57 -0400

    Adding photon+jet / diphoton skim.

--- monophoton/phoMet/skim.py
+++ monophoton/phoMet/skim.py
@@ -13,8 +13,8 @@ import config
 ROOT.gSystem.Load('libMitFlatDataFormats.so')
 ROOT.gSystem.AddIncludePath('-I' + os.environ['CMSSW_BASE'] + '/src/MitFlat/DataFormats/interface')
 
-skim = "skim"
-sname = sys.argv[1]
+skim = sys.argv[1]
+sname = sys.argv[2]
 
 samples = [allsamples[sname]] # for sname in snames ] 
 
