fc8cc39c8905f1c5b1e236ba674288dd677574e1
Yutaro Iiyama <yiiyama@mit.edu>
2016-05-03 09:33:54 -0400

    more efake code

---
+++ monophoton/tp/efake_mcbkg.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import array
+
+thisdir = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.dirname(thisdir)
+sys.path.append(basedir)
+from datasets import allsamples
+from plotstyle import SimpleCanvas
+
+import ROOT
+ROOT.gROOT.SetBatch(True)
+
+ROOT.gSystem.Load('libMitFlatDataFormats.so')
+ROOT.gSystem.AddIncludePath('-I' + os.environ['CMSSW_BASE'] + '/src/MitFlat/DataFormats/interface')
+ROOT.gROOT.LoadMacro(thisdir + '/TemplateGenerator.cc+')
+
+inputDir = '/scratch5/yiiyama/studies/egfake_skim'
+
+groups = {
+    'dy': (['dy-50'], ROOT.TColor.GetColor(0xff, 0x88, 0x44)),
+    'top': (['tt'], ROOT.TColor.GetColor(0x66, 0xff, 0xdd))
+    'w': (['wlnu-100', 'wlnu-200', 'wlnu-400', 'wlnu-600'], ROOT.TColor.GetColor(0xaa, 0xff, 0x77)),
+    'vv': (['ww', 'wz', 'zz'], ROOT.TColor.GetColor(0xaa, 0x55, 0xff))
+}
+
+for gname in ['vv', 'top', 'w', 'dy']:
+    samples, color = groups[gname]
+
+    egTree = ROOT.TChain('events')
+    mgTree = ROOT.TChain('events')
+    for sname in samples:
+        egTree.Add(inputDir + '/' + )
+    
