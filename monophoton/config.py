2d0df20a32bd7dc0434b2b4e254a89f7fe196317
Brandon <ballen@mit.edu>
2016-05-10 04:44:07 -0400

    Adding config.py to protect against changes.

--- monophoton/config.py
+++ monophoton/config.py
@@ -1,4 +1,6 @@
 skimDir = '/scratch5/ballen/hist/monophoton/skim'
 histDir = '/home/ballen/cms/root'
-ntuplesDir = '/scratch5/yiiyama/hist/simpletree12/t2mit/filefi/042/'
-phskimDir = '/scratch5/yiiyama/hist/simpletree12/phskim/'
+ntuplesDir = '/scratch5/yiiyama/hist/simpletree13c/t2mit/'
+dataNtuplesDir = ntuplesDir.replace('13c', '14') 
+phskimDir = '/scratch5/yiiyama/hist/simpletree13c/phskim/'
+webDir = '/home/ballen/public_html/cmsplots/monophoton'
