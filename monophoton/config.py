70db6246c42e566ce74a2bbb5d609cd082a16506
Brandon <ballen@mit.edu>
2016-06-01 13:06:17 -0400

    Added MC to the Z+jet studies.

--- monophoton/config.py
+++ monophoton/config.py
@@ -1,14 +1,15 @@
 skimDir = '/scratch5/ballen/hist/monophoton/skim'
 histDir = '/home/ballen/cms/root'
 ntuplesDir = '/scratch5/yiiyama/hist/simpletree13c/t2mit/'
-dataNtuplesDir = ntuplesDir.replace('13c', '14') 
+dataNtuplesDir = ntuplesDir # .replace('13c', '14') 
 phskimDir = '/scratch5/yiiyama/hist/simpletree13c/phskim/'
 webDir = '/home/ballen/public_html/cmsplots/monophoton'
 
 import os
-thisdir = os.path.dirname(os.path.realpath(__file__))
-basedir = os.path.dirname(thisdir)
+basedir = os.path.dirname(os.path.realpath(__file__))
 
-libsimpletree = 'libMitFlatDataFormats.so'
-dataformats = os.environ['CMSSW_BASE'] + '/src/MitFlat/DataFormats'
+# libsimpletree = 'libMitFlatDataFormats.so'
+# dataformats = os.environ['CMSSW_BASE'] + '/src/MitFlat/DataFormats'
 
+libsimpletree = basedir + '/MitFlat/DataFormats/obj/libsimpletree.so'
+dataformats = basedir + '/MitFlat/DataFormats'
--- monophoton/phoMet/plotZ.py
+++ monophoton/phoMet/plotZ.py
@@ -7,7 +7,7 @@ import array
 thisdir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(thisdir)
 sys.path.append(basedir)
-from plotstyle import SimpleCanvas
+from plotstyle import DataMCCanvas
 from datasets import allsamples
 from main.plotconfig import VariableDef
 
@@ -15,7 +15,7 @@ import ROOT as r
 r.gROOT.SetBatch(True)
 
 lumi = allsamples['sel-d3'].lumi + allsamples['sel-d4'].lumi
-canvas = SimpleCanvas(lumi = lumi)
+canvas = DataMCCanvas(lumi = lumi)
 
 probePixel = '!probe.pixelVeto'
 zMassCut = 'z.mass > 81. && z.mass < 101.'
@@ -35,48 +35,79 @@ variables = [ VariableDef('Met', 'E_{T}^{miss}', 't1Met.met', [10 * x for x in r
               VariableDef('minDPhiJetMet', 'min #Delta#phi(jet, E_{T}^{miss})', 't1Met.minJetDPhi', (15, 0., math.pi))
               ]
 
-cuts = [ dPhiCut, metCut, zMassCut, njetsCut ]
+baseCuts = [ dPhiCut, metCut, zMassCut ]
 
 zSignCuts = [ ('os', 'z.oppSign == 1'), ('ss', 'z.oppSign == 0') ]
 
+jetsCuts = [ ('monojet30', [njetsCut]),
+            ('monojet100', [njetsCut, jetPtCut]),
+            ('multijet', [jetPtCut]),
+            ('multijetdPhiCut', [jetPtCut, dPhiJetCut])
+            ]
+
 skims = [ 'zmumu_smu', 'zee_sel', 'z*_s*' ]
+
+samples = [ ('zllg', r.TColor.GetColor(0xff, 0x99, 0x33)), 
+            ('ttg', r.TColor.GetColor(0xbb, 0xaa, 0xff)), 
+            ('wlnu-', r.TColor.GetColor(0xff, 0xee, 0x99)), 
+            ('wg', r.TColor.GetColor(0x99, 0xee, 0xff)), 
+            ('tt', r.TColor.GetColor(0xff, 0xaa, 0xcc)),
+            ('dy-50-', r.TColor.GetColor(0x99, 0xff, 0xaa)) 
+            ]
+
 
 for skim in skims:
     dataTree = r.TChain('skim')
     dataTree.Add('/scratch5/ballen/hist/monophoton/phoMet/'+skim+'-*.root')
     print dataTree.GetEntries()
-    
-    for sign, cut in zSignCuts[:1]:
-        signCuts = cuts + [cut]
 
-        for varDef in variables:
-            if varDef.applyFullSel:
-                varCuts = signCuts + [ dPhiMetCut ]
-            else:
-                varCuts = signCuts
+    skimm = skim.split('_')[0]
 
-            cutString = ' && '.join(['(%s)' % c for c in varCuts])
-            print cutString
+    mcTrees = []
+    for sample, color in samples:
+        mcTree = r.TChain('skim')
+        mcTree.Add('/scratch5/ballen/hist/monophoton/phoMet/'+skimm+'_'+sample+'*.root')
+        mcTrees.append( (sample, color, mcTree) )
 
-            canvas.Clear()
-            canvas.legend.Clear()
-            canvas.legend.setPosition(0.6, 0.7, 0.9, 0.9)
+    for jetsCut in jetsCuts:
+        cuts = baseCuts + jetsCut[1]
+            
+        for sign, cut in zSignCuts[:1]:
+            signCuts = cuts + [cut]
 
-            dataName = 'dataHist'+skim
-            dataHist = varDef.makeHist(dataName)
+            for varDef in variables:
+                if varDef.applyFullSel:
+                    varCuts = signCuts + [ dPhiMetCut ]
+                else:
+                    varCuts = signCuts
 
-            if varDef.unit != '':
-                dataHist.GetXaxis().SetTitle(varDef.title + ' (' + varDef.unit +')')
-            else:
-                dataHist.GetXaxis().SetTitle(varDef.title)
+                cutString = ' && '.join(['(%s)' % c for c in varCuts])
+                print cutString
 
-            dataTree.Draw(varDef.expr+'>>'+varDef.name+'-'+dataName, 'weight * ('+cutString+')', 'goff')
-            print dataHist.Integral()
+                canvas.Clear()
+                canvas.legend.Clear()
+                canvas.legend.setPosition(0.6, 0.7, 0.9, 0.9)
 
-            canvas.legend.add('data', title = 'Data '+skim, mcolor = r.kBlack, lcolor = r.kBlack, lwidth = 1)
-            canvas.legend.apply('data', dataHist)
-            canvas.addHistogram(dataHist, drawOpt = 'EP')
+                dataName = 'dataHist'+skim
+                dataHist = varDef.makeHist(dataName)
 
-            canvas.ylimits = (0.001, -1.)
+                if varDef.unit != '':
+                    dataHist.GetXaxis().SetTitle(varDef.title + ' (' + varDef.unit +')')
+                else:
+                    dataHist.GetXaxis().SetTitle(varDef.title)
 
-            canvas.printWeb('monophoton/phoMet/'+skim, sign+'_monojet30_'+varDef.name, logy = False)
+                dataTree.Draw(varDef.expr+'>>'+varDef.name+'-'+dataName, 'weight * ('+cutString+')', 'goff')
+                print dataHist.Integral()
+                canvas.addObs(dataHist, title = 'Data '+skimm)
+
+                for sample, color, mcTree in mcTrees:
+                    mcName = sample+skimm
+                    mcHist = varDef.makeHist(mcName)
+                    mcTree.Draw(varDef.expr+'>>'+varDef.name+'-'+mcName, str(lumi)+' * weight * ('+cutString+')', 'goff')
+                    canvas.addStacked(mcHist, title = sample, color = color)
+
+                # canvas.ylimits = (0.001, -1.)
+                canvas.xtitle = varDef.title
+                canvas.ytitle = 'Events'
+
+                canvas.printWeb('monophoton/phoMet/'+skim, sign+'_'+jetsCut[0]+'_'+varDef.name, logy = False)
