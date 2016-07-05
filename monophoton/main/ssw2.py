e0eea2c8246ccac97827096f29c50b663289e284
Yutaro Iiyama <yiiyama@mit.edu>
2016-07-05 19:19:46 -0400

    gjets raw -> monoph

--- monophoton/main/ssw2.py
+++ monophoton/main/ssw2.py
@@ -47,8 +47,8 @@ mc_dilep = ['dimu', 'diel', 'elmu']
 mc_vgcand = [(region, selectors.kfactor(defaults[region])) for region in mc_cand]
 mc_vglep = [(region, selectors.kfactor(defaults[region])) for region in mc_lep]
 mc_vgdilep = [(region, selectors.kfactor(defaults[region])) for region in mc_dilep]
-#mc_gj = [('monoph', selectors.kfactor(selectors.gjSmeared)), ('purity', selectors.kfactor(selectors.purity))]
-mc_gj = [('monoph', selectors.kfactor(defaults['monoph'])), ('purity', selectors.kfactor(selectors.purity))]
+#mc_gj = [('raw', selectors.kfactor(defaults['monoph'])), ('monoph', selectors.kfactor(selectors.gjSmeared)), ('purity', selectors.kfactor(selectors.purity))]
+mc_gj = [('raw', selectors.kfactor(defaults['monoph'])), ('monoph', selectors.kfactor(defaults['monoph'])), ('purity', selectors.kfactor(selectors.purity))]
 mc_wlnu = [(region, selectors.wlnu(defaults[region])) for region in mc_cand] + ['wenu']
 mc_lowmt = ['lowmt']
 mc_vglowmt = [(region, selectors.kfactor(defaults[region])) for region in mc_lowmt]
@@ -59,6 +59,10 @@ haloNorms = [ 7.3 * allsamples[sph].lumi / sphLumi for sph in ['sph-16b2'] ]
 selectors = {
     # Data 2016
     'sph-16b2': data_sph + [('halo', selectors.haloMIP(haloNorms[0]))
+                            ,('haloUp', selectors.haloCSC(haloNorms[0]))
+                            ,('haloDown', selectors.haloSieie(haloNorms[0]))
+                             ],
+    'sph-16b2s': data_sph + [('halo', selectors.haloMIP(haloNorms[0]))
                             ,('haloUp', selectors.haloCSC(haloNorms[0]))
                             ,('haloDown', selectors.haloSieie(haloNorms[0]))
                              ],
