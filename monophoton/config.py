674cf7f4e91a8aceea72c2812d36b9c61ee7c74b
Brandon <ballen@mit.edu>
2016-07-13 07:53:27 -0400

    changing lumi cut off to the july 8th json.

--- monophoton/config.py
+++ monophoton/config.py
@@ -11,9 +11,9 @@ import json
 # 274421 ~ june 10 ~ 2.3/fb unblind - fully unblinded
 # 274443 ~ june 16 ~ 2.8/fb unblind - 2.4/fb blind
 # 275125 ~ june 22 ~ 4.1/fb unblind - 2.7/fb blind
-# 275583 ~ july 08 ~
+# 275583 ~ july 08 ~ 5.5/fb unblund - !! not full json lumi
 blindCutOff = '274421'
-runCutOff = '274443'
+runCutOff = '275583'
 jsonLumi = 0.
 jsonLumiBlinded = 0.
 with open(basedir + '/data/lumis.txt') as lumiFile:
