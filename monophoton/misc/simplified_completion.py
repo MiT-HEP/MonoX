de77aba3f4b8cd4e71a26864819d3f8ac1fc0944
Yutaro Iiyama <yiiyama@mit.edu>
2016-07-20 14:34:22 -0400

    fastsim signal tools

--- monophoton/misc/simplified_completion.py
+++ monophoton/misc/simplified_completion.py
@@ -1,59 +1,72 @@
 import os
+import sys
 import re
 import collections
 import subprocess
 
 server = 'root://xrootd.cmsaf.mit.edu/'
-directory = '/store/user/paus/fastsm/043/'
-command = 'source /cvmfs/cms.cern.ch/cmsset_default.sh; cd ' + os.environ['CMSSW_BASE'] + '; eval `scram runtime -sh`; xrdfs %s ls %s/monophoton_{point} | sort | uniq' % (server, directory)
+directory = '/store/user/paus/fastsm/043'
+
+def xrdls(path):
+    command = 'source /cvmfs/cms.cern.ch/cmsset_default.sh; cd ' + os.environ['CMSSW_BASE'] + '; eval `scram runtime -sh`; xrdfs %s ls {path} | sort | uniq' % server
+
+    proc = subprocess.Popen(command.format(path = path), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    out, err = proc.communicate()
+
+    if err:
+        print err
+
+    return out.split('\n')
 
 output = open('simplified_fastsim.txt', 'w')
 
-with open('../data/fastsim_list.txt') as source:
-    for line in source:
-        matches = re.match('(med-([0-9]+)_dm-([0-9]+)_cv-([01]\.0)_ca-[01]\.0)_nev-50000', line)
+directories = xrdls(directory)
+source = []
+for d in directories:
+    if 'monophoton' in d:
+        source.append(d.replace(directory, '').replace('/', ''))
+
+for line in source:
+    matches = re.match('monophoton_med-([0-9]+)_dm-([0-9]+)_cv-([01]\.0)_ca-[01]\.0', line)
+    if not matches:
+        continue
+
+    point = matches.group(0)
+    med = int(matches.group(1))
+    dm = int(matches.group(2))
+    cv = float(matches.group(3))
+
+    print point
+
+    fnames = filter(lambda f: 'miniaodsim' in f, xrdls(directory + '/' + point))
+
+    flist = collections.defaultdict(list)
+    for fname in fnames:
+        matches = re.match('/.*/' + point + '_nev-([0-9]+)_seed-([0-9]+)_miniaodsim.root', fname)
         if not matches:
+            print fname
             continue
 
-        point = matches.group(1)
-        med = int(matches.group(2))
-        dm = int(matches.group(3))
-        cv = float(matches.group(4))
+        flist[int(matches.group(2))].append((int(matches.group(1)), fname))
 
-        print point
+    total = 0
+    block = ''
+    for seed in sorted(flist.keys()):
+        best = ''
+        bestn = 0
+        for nev, fname in flist[seed]:
+            if nev > bestn:
+                best = fname
+                bestn = nev
 
-        proc = subprocess.Popen(command.format(point = point), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-        out, err = proc.communicate()
+        total += bestn
+        flist[seed].remove((bestn, best))
 
-        fnames = filter(lambda f: 'miniaodsim' in f, out.split('\n'))
+        block += server + best.replace('//', '/') + '\n'
 
-        flist = collections.defaultdict(list)
-        for fname in fnames:
-            matches = re.match('/.*/monophoton_med-[0-9]+_dm-[0-9]+_cv-.+_ca-.+_nev-([0-9]+)_seed-([0-9]+)_miniaodsim.root', fname)
-            if not matches:
-                print fname
-                continue
-
-            flist[int(matches.group(2))].append((int(matches.group(1)), fname))
-
-        total = 0
-        block = ''
-        for seed in sorted(flist.keys()):
-            best = ''
-            bestn = 0
-            for nev, fname in flist[seed]:
-                if nev > bestn:
-                    best = fname
-                    bestn = nev
-
-            total += bestn
-            flist[seed].remove((bestn, best))
-
-            block += server + best.replace('//', '/') + '\n'
-
-            for nev, fname in flist[seed]:
-                block += '#' + server + fname.replace('//', '/') + '\n'
-            
-        output.write('[med-%d_dm-%d %s] %d events\n' % (med, dm, 'V' if cv == 1. else 'AV', total))
-        output.write(block)
-        output.write('\n')
+        for nev, fname in flist[seed]:
+            block += '#' + server + fname.replace('//', '/') + '\n'
+        
+    output.write('[med-%d_dm-%d %s] %d events\n' % (med, dm, 'V' if cv == 1. else 'AV', total))
+    output.write(block)
+    output.write('\n')
--- monophoton/misc/simplified_xsec.py
+++ monophoton/misc/simplified_xsec.py
@@ -3,13 +3,18 @@
 import os
 import sys
 import subprocess
+import shutil
 
 thisdir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(thisdir)
 sys.path.append(basedir)
 import config
 
-point = ' '.join(sys.argv[1:])
+# sed -n 's/\[\(med-[^ ]*\) \(.*\)\] [1-9][0-9]* events/\1_\2/p' simplified_fastsim.txt | while read line; do [ -e /scratch5/yiiyama/studies/monophoton16/simplified_xsec/$line.dat ] || echo $line >> ~/tmp/points.txt; done
+# condor-run $PWD/simplified_xsec.py -a ~/tmp/points.txt
+
+# med-X_dm-Y_V/AV
+point = sys.argv[1]
 
 outdir = config.histDir + '/simplified_xsec'
 
@@ -30,15 +35,17 @@ if point == 'combine':
             output.write(point + ' ' + values[point] + '\n')
 
 else:
-    outname = outdir + '/' + point.replace('[', '').replace(']', '').replace(' ', '_') + '.dat'
+    outname = outdir + '/' + point + '.dat'
     if os.path.exists(outname):
         sys.exit(0)
 
+    header = '[' + point[:point.rfind('_')] + ' ' + point[point.rfind('_') + 1:] + ']'
+
     files = None
-    with open('simplified_fastsim.txt') as flist:
+    with open(basedir + '/data/simplified_fastsim.txt') as flist:
         for line in flist:
             line = line.strip()
-            if line.startswith(point):
+            if line.startswith(header):
                 files = []
                 continue
     
@@ -49,13 +56,35 @@ else:
                 break
     
             files.append(line)
+
+    if files is None:
+        sys.exit(1)
     
     inputFiles = ' '.join(['inputFiles=%s' % f for f in files])
-    
+
     output = open(outname, 'w')
-    
-    proc = subprocess.Popen('source /cvmfs/cms.cern.ch/cmsset_default.sh;export X509_USER_PROXY=/home/' + os.environ['USER'] + '/x509up_u' + str(os.getuid()) + '; cd ' + os.environ['CMSSW_BASE'] + ';eval `scram runtime -sh`;cmsRun genxsec.py ' + inputFiles, shell = True, stdout = output, stderr = subprocess.STDOUT)
+
+    commands = [
+        'source /cvmfs/cms.cern.ch/cmsset_default.sh',
+        'export X509_USER_PROXY=$PWD/x509up_u%d' % os.getuid(),
+        'cd ' + os.environ['CMSSW_BASE'],
+        'eval `scram runtime -sh`'
+        'cmsRun /home/yiiyama/cms/cmssw/cfg/genxsec.py ' + inputFiles
+    ]
+
+    proc = subprocess.Popen(';'.join(commands), shell = True, stdout = output, stderr = subprocess.STDOUT)
     
     proc.communicate()
     
     output.close()
+
+    output = open(outname)
+
+    for line in output:
+        if 'final cross section' in line:
+            output.close()
+            break
+
+    else:
+        output.close()
+        os.remove(outname)
