69636b84a695d91bb1f6c8891612c88b9d27fad8
Brandon <ballen@mit.edu>
2016-10-11 12:35:55 -0400

    Added command line control for region selection for MC eff calculation. Results seem to be off by a factor of 2. need to debug =(

--- monophoton/purity/calcMcEff.py
+++ monophoton/purity/calcMcEff.py
@@ -13,6 +13,14 @@ import config
 ROOT.gSystem.Load(config.libsimpletree)
 ROOT.gSystem.AddIncludePath('-I' + config.dataformats + '/interface')
 
+loc = sys.argv[1]
+pid = sys.argv[2]
+chiso = sys.argv[3]
+pt = sys.argv[4]
+met = sys.argv[5]
+
+inputKey = loc+'_'+pid+'_ChIso'+chiso+'_PhotonPt'+pt+'_Met'+met
+
 source = ROOT.TChain('events')
 print config.skimDir
 source.Add(config.skimDir + '/gj-40_purity.root')
@@ -26,10 +34,33 @@ ROOT.gROOT.LoadMacro(thisdir + '/Calculator.cc+')
 print source.GetEntries()
 
 calc = ROOT.Calculator()
-calc.setMinPhoPt(175.)
-# calc.setMaxPhoPt(200.)
 calc.setMaxDR(0.2)
 calc.setMaxDPt(0.2)
+
+if loc == 'barrel':
+    minEta = 0.
+    maxEta = 1.5
+elif loc == 'endcap':
+    minEta = 1.5
+    maxEta = 2.5
+calc.setMinEta(minEta)
+calc.setMaxEta(maxEta)
+
+idmap = { 'loose' : 0, 'medium' : 1, 'tight' : 2 }
+calc.setWorkingPoint(idmap[pid])
+
+(minPt, maxPt) = pt.split('to')
+if maxPt == 'Inf':
+    maxPt = 6500.
+calc.setMinPhoPt(float(minPt))
+calc.setMaxPhoPt(float(maxPt))
+
+(minMet, maxMet) = met.split('to')
+if maxMet == 'Inf':
+    maxMet = 6500.
+calc.setMinMet(float(minMet))
+calc.setMaxMet(float(maxMet))
+
 calc.calculate(source)
 
 effs= []
