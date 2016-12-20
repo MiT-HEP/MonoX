a6395e5c5e2274bf69ae7bf5476d4bc51131f4a6
Yutaro Iiyama <yiiyama@mit.edu>
2016-12-19 23:16:52 -0500

    checking in

--- monophoton/config.py
+++ monophoton/config.py
@@ -1,11 +1,11 @@
 import os
 
 # tier3 set up
-skimDir = '/data/t3home000/ballen/hist/monophoton/skim'
-histDir = '/data/t3home000/ballen/hist/monophoton'
+skimDir = '/data/t3home000/yiiyama/studies/monophoton/skim'
+histDir = '/data/t3home000/yiiyama/studies/monophoton'
 ntuplesDir = '/data/t3serv014/yiiyama/hist/simpletree19/t2mit/'
 dataNtuplesDir = ntuplesDir
-photonSkimDir = '/data/t3home000/yiiyama/simpletree19/photonskim_newJSON'
+photonSkimDir = '/data/t3home000/yiiyama/simpletree19/photonskim_newCH'
 
 libsimpletree = 'libMitFlatDataFormats.so'
 dataformats = os.environ['CMSSW_BASE'] + '/src/MitFlat/DataFormats'
