49c2230c52aebca6195b1763bf0f9a3e04487b3e
Brandon <ballen@mit.edu>
2017-01-30 08:24:12 -0500

    added data/MC TF comparison to visualizeWS.py

--- monophoton/fit/visualizeWS.py
+++ monophoton/fit/visualizeWS.py
@@ -12,7 +12,7 @@ plotDir = 'workspace'
 thisdir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(thisdir)
 sys.path.append(basedir)
-from plotstyle import SimpleCanvas
+from plotstyle import SimpleCanvas, RatioCanvas
 
 import ROOT
 
@@ -26,13 +26,20 @@ canvas1.legend.add('stat', 'stat.', opt = 'L', color = ROOT.kBlack, mstyle = 8)
 
 source = ROOT.TFile.Open(parameters.plotsOutname)
 
+tfList = ['tf_' + target[0] + '_' + target[1] + '_' + base[0] + '_' + base[1]  for (target, base) in parameters.links]
+print tfList
+procList = parameters.processes + parameters.signals + tfList
+
 for region in parameters.regions:
-    for proc in parameters.processes + parameters.signals + ['tf_' + p for p in parameters.processes]:
-        hnominal = source.Get(proc + '_' + region)
+    for proc in procList:
+        if 'tf_' in proc:
+            hnominal = source.Get(proc)
+            huncert = source.Get(proc + '_uncertainties')
+        else:
+            hnominal = source.Get(proc + '_' + region)
+            huncert = source.Get(proc + '_' + region + '_uncertainties')
         if not hnominal:
             continue
-
-        huncert = source.Get(proc + '_' + region + '_uncertainties')
     
         canvas1.legend.apply('stat', hnominal)
     
@@ -47,3 +54,35 @@ for region in parameters.regions:
             canvas2.Clear()
             canvas2.addHistogram(hnominal, drawOpt = 'EP')
             canvas2.printWeb(plotDir, hnominal.GetName(), logy = not proc.startswith('tf_'))
+
+rcanvas = RatioCanvas(name = 'datamc', lumi = 36400)
+rcanvas.legend.setPosition(0.7, 0.7, 0.9, 0.9)
+
+for lep in ['mu', 'el']:
+    for tf in tfList:
+        if lep in tf:
+            hMcRatio = source.Get(tf)
+            hMcRatioUnc = source.Get(tf + '_uncertainties')
+
+    hDilepData = source.Get('data_obs_di' + lep + '__x')
+    hMonolepData = source.Get('data_obs_mono' + lep + '__x')
+    
+    hDataRatio = hDilepData.Clone('tf_data_'+lep)
+    hDataRatio.Divide(hMonolepData)
+
+    rcanvas.Clear()
+    rcanvas.legend.Clear()
+    rcanvas.legend.add('mc', 'Z/W MC', opt = 'L', color = ROOT.kBlue + 1)
+    rcanvas.legend.add('mcUnc', 'Z/W MC Uncertainty', opt = 'F', color = ROOT.kOrange + 1, fstyle = 1001)
+    rcanvas.legend.add('data', 'Z/W Data', opt = 'L', color = ROOT.kBlack, mstyle = 8)
+
+    rcanvas.legend.apply('mc', hMcRatio)
+    rcanvas.legend.apply('mcUnc', hMcRatioUnc)
+    rcanvas.legend.apply('data', hDataRatio)
+
+    iMc = rcanvas.addHistogram(hMcRatio, drawOpt = 'HIST')
+    iUnc = rcanvas.addHistogram(hMcRatioUnc, drawOpt = 'E2')
+    iData = rcanvas.addHistogram(hDataRatio, drawOpt = 'EP')
+    
+    outName = 'tf_ratio_'+lep
+    rcanvas.printWeb(plotDir, outName, hList = [iUnc, iMc, iData], rList = [iMc, iUnc, iData], logy = False)
