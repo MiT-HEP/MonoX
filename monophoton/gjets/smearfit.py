3506c8743fb4e045a06cdac638ad98c857f344fa
Brandon <ballen@mit.edu>
2017-02-03 13:45:20 -0500

    updated gjets smearfit for 2016 data. need to update once we have low met zg samples. also maybe add a loop over resolution function choices.

--- monophoton/gjets/smearfit.py
+++ monophoton/gjets/smearfit.py
@@ -15,13 +15,13 @@ ROOT.gSystem.Load('libRooFit.so')
 ROOT.gSystem.Load('/home/yiiyama/cms/studies/RooFit/libCommonRooFit.so')
 ROOT.gROOT.LoadMacro('metTree.cc+')
 
-photonData = ['sph-16b-r', 'sph-16c-r', 'sph-16d-r', 'sph-16e-r', 'sph-16f-r', 'sph-16g-r', 'sph-16h']
+photonData = ['sph-16b-r', 'sph-16c-r', 'sph-16d-r', 'sph-16e-r', 'sph-16f-r', 'sph-16g-r', 'sph-16h'] # ['sph-16c-r'] #
 
 lumi = 0.
 for sname in photonData:
     lumi += allsamples[sname].lumi
 
-canvas = RatioCanvas(cms = False)
+canvas = RatioCanvas(lumi = lumi)
 
 # direct smear
 
@@ -38,10 +38,11 @@ bmcsource = ROOT.TChain('events')
 bmcsource.Add(config.skimDir + '/wglo_monoph.root') # inclusive sample - wg130 canno be used because we go lower in MET
 bmcsource.Add(config.skimDir + '/wlnu-*_monoph.root')
 bmcsource.Add(config.skimDir + '/ttg_monoph.root')
-bmcsource.Add(config.skimDir + '/zg_monoph.root')
+bmcsource.Add(config.skimDir + '/znng-130_monoph.root') # temporaryyyy
+bmcsource.Add(config.skimDir + '/zllg-130_monoph.root') # temporaryyyy
 
-znnsource = ROOT.TChain('events')
-znnsource.Add(config.skimDir + '/zg_dimu.root')
+# znnsource = ROOT.TChain('events')
+# znnsource.Add(config.skimDir + '/zg_dimu.root')  # need to replace with new sample
 
 mcsource = ROOT.TChain('events')
 mcsource.Add(config.skimDir + '/gj-40_monoph.root')
@@ -61,7 +62,7 @@ dsource.Draw('t1Met.met>>'+dname, sel, 'goff')
 btree = ROOT.TTree('btree', 'met')
 ROOT.metTree(bsource, btree, sel)
 ROOT.metTree(bmcsource, btree, sel, lumi)
-ROOT.metTree(znnsource, btree, sel, lumi * 6.122)
+# ROOT.metTree(znnsource, btree, sel, lumi * 6.122)
 
 counter = ROOT.TH1D('counter', '', 1, 0., 1.)
 btree.Draw('0.5>>counter', 'weight', 'goff')
@@ -85,7 +86,26 @@ mcname = 'mcmetLow'
 mcpdf = ROOT.KeysShape('mcpdf', 'mcpdf', met, mctree, 'weight', 0.5, 8)
 print 'Done.'
 
-smear = space.factory('Landau::smear(met, mean[0., -1., 10.], sigmar[1., 0., 100.])')
+sigmar = space.factory('sigmar[1., 0., 100.]')
+alpha = space.factory('alpha[0.1, 0., 1.]')
+beta = space.factory('beta[0.1, 0., 1.]')
+mean = space.factory('mean[0, -1., 50.]')
+
+print sigmar
+print alpha
+print beta
+print mean
+
+print 'making width'
+width = space.factory('expr::width("@0*(1. + @1*@3 + @2*@3*@3)", {{sigmar, alpha, beta, met}})')
+widthName = 'quadratic'
+# width = space.factory('expr::width("@0*(1. + @1*@2)", {{sigmar, alpha, met}})')
+# widthName = 'linear'
+print 'width made'
+
+print 'making smear'
+smear = space.factory('Landau::smear(met, mean, width)')
+print 'smear made'
 
 gjets = ROOT.RooFFTConvPdf('gjets', 'gjets', met, mcpdf, smear)
 
@@ -96,12 +116,19 @@ model = ROOT.RooAddPdf('model', 'model', ROOT.RooArgList(gjets, bpdf), ROOT.RooA
 
 model.fitTo(ddata)
 
+paramsOut = file('params_' + widthName + '.txt', 'w')
+paramsOut.write('%10s: %10.3g %10.3g \n' % ('sigmar', sigmar.getValV(), sigmar.getError()))
+paramsOut.write('%10s: %10.3g %10.3g \n' % ('alpha', alpha.getValV(), alpha.getError()))
+paramsOut.write('%10s: %10.3g %10.3g \n' % ('beta', beta.getValV(), beta.getError()))
+paramsOut.write('%10s: %10.3g %10.3g \n' % ('mean', mean.getValV(), mean.getError()))
+paramsOut.close()
+
 frame = met.frame()
 ddata.plotOn(frame)
 model.plotOn(frame)
 model.plotOn(frame, ROOT.RooFit.Components('gjets'), ROOT.RooFit.LineColor(ROOT.kGreen), ROOT.RooFit.LineStyle(ROOT.kDashed))
 mcpdf.plotOn(frame, ROOT.RooFit.LineColor(ROOT.kRed), ROOT.RooFit.LineStyle(ROOT.kDotted))
-frame.SetTitle('min#Delta#phi(j, E_{T}^{miss}) < 0.5')
+# frame.SetTitle('min#Delta#phi(j, E_{T}^{miss}) < 0.5')
 #frame.GetXaxis().SetTitle('E_{T}^{miss} (GeV)')
 frame.GetXaxis().SetTitle('')
 frame.GetXaxis().SetTitleSize(0.)
@@ -115,9 +142,9 @@ canvas.legend.add('fit', title = 'Fit', color = ROOT.kBlue, lwidth = 2, opt = 'L
 canvas.legend.add('gjets', title = '#gamma+jets', color = ROOT.kGreen, lstyle = ROOT.kDashed, lwidth = 2, opt = 'L')
 canvas.legend.add('mcpdf', title = '#gamma+jets raw', color = ROOT.kRed, lstyle = ROOT.kDotted, lwidth = 2, opt = 'L')
 
-canvas.addHistogram(frame, clone = False, drawOpt = '')
+canvas.addHistogram(frame, clone = True, drawOpt = '')
 
-canvas.Update()
+canvas.Update(rList = [])
 
 fitcurve = frame.findObject('model_Norm[met]')
 rawcurve = frame.findObject('mcpdf_Norm[met]')
@@ -175,7 +202,7 @@ rdata.Draw('EP')
 
 canvas._needUpdate = False
 
-canvas.printWeb('smearfit', 'direct')
+canvas.printWeb('monophoton/smearfit', widthName)
 
 outputFile.cd()
 space.Write()
