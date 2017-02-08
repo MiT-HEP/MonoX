695b8aab76816ebdadeb20682e5336856c3f77ac
Brandon <ballen@mit.edu>
2017-02-08 11:36:33 -0500

    moving back to where most data is.;

--- monophoton/config.py
+++ monophoton/config.py
@@ -3,7 +3,7 @@ import os
 # tier3 set up
 skimDir = '/data/t3home000/' + os.environ['USER'] + '/studies/monophoton/skim'
 histDir = '/data/t3home000/' + os.environ['USER'] + '/studies/monophoton'
-ntuplesDir = '/mnt/hadoop/scratch/yiiyama/simpletree19/t2mit/'
+ntuplesDir = '/data/t3serv014/yiiyama/hist/simpletree19/t2mit/'
 dataNtuplesDir = ntuplesDir
 photonSkimDir = '/mnt/hadoop/scratch/yiiyama/photonskim'
 
