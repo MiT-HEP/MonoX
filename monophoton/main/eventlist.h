00703006e92bce75cd9d82e977b494290abb21d4
Brandon <ballen@mit.edu>
2017-02-09 13:32:45 -0500

    updated eventlist.h to use panda

--- monophoton/main/eventlist.h
+++ monophoton/main/eventlist.h
@@ -31,15 +31,15 @@ class EventList {
 
   bool inList(panda::Event const& _event) const
   {
-    auto rItr(list_.find(_event.run));
+    auto rItr(list_.find(_event.runNumber));
     if (rItr == list_.end())
       return false;
 
-    auto lItr(rItr->second.find(_event.lumi));
+    auto lItr(rItr->second.find(_event.lumiNumber));
     if (lItr == rItr->second.end())
       return false;
 
-    auto eItr(lItr->second.find(_event.event));
+    auto eItr(lItr->second.find(_event.eventNumber));
     return eItr != lItr->second.end();
   }
 
