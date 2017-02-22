2ac3c5e7280359725d47150d6814dc5df1004435
Yutaro Iiyama <yiiyama@mit.edu>
2017-02-22 01:40:12 -0500

    reorganized photonskim

--- monophoton/misc/PhotonSkim.cc
+++ monophoton/misc/PhotonSkim.cc
@@ -11,10 +11,14 @@ class PhotonSkimmer {
 public:
   PhotonSkimmer();
   void addSourcePath(char const* path) { merger_.addInput(path); }
-  void run(char const*, long = -1, GoodLumiFilter* = 0);
+  void setLumiFilter(GoodLumiFilter* f) { goodlumi_ = f; }
+  void run(char const*, bool doSkim, long = -1);
+
+  bool skimFunction(panda::Event&, panda::EventMonophoton&);
 
 private:
   panda::FileMerger merger_;
+  GoodLumiFilter* goodlumi_{0};
 };
 
 PhotonSkimmer::PhotonSkimmer()
@@ -24,119 +28,24 @@ PhotonSkimmer::PhotonSkimmer()
 }
 
 void
-PhotonSkimmer::run(char const* _outputPath, long _nEvents/* = -1*/, GoodLumiFilter* _goodlumi/* = 0*/)
+PhotonSkimmer::run(char const* _outputPath, bool _doSkim, long _nEvents/* = -1*/)
 {
-  panda::EventMonophoton outEvent;
-  merger_.setOutEvent(&outEvent);
+  panda::EventMonophoton monophEvent;
 
-  merger_.setEventSelection("TMath::Abs(superClusters.eta) < 1.4442 && superClusters.rawPt > 150.");
-
-  panda::FileMerger::SkimFunction skim([&outEvent, _goodlumi](panda::Event& _event)->bool {
-      if (_goodlumi && !_goodlumi->isGoodLumi(_event.runNumber, _event.lumiNumber))
-        return false;
-
-      unsigned iPh(0);
-      for (; iPh != _event.photons.size(); ++iPh) {
-        auto& photon(_event.photons[iPh]);
-        if (std::abs(photon.superCluster->eta) < 1.4442 && photon.superCluster->rawPt > 150.)
-          break;
-      }
-      if (iPh == _event.photons.size())
-        return false;
-
-      // copy most of the event content (special operator= of EventMonophoton that takes Event as RHS)
-      outEvent = _event;
-  
-      for (unsigned iPh(0); iPh != _event.photons.size(); ++iPh) {
-        auto& inPhoton(_event.photons[iPh]);
-        auto& superCluster(*inPhoton.superCluster);
-        auto& outPhoton(outEvent.photons[iPh]);
-
-        outPhoton.scRawPt = superCluster.rawPt;
-        outPhoton.scEta = superCluster.eta;
-        outPhoton.e4 = inPhoton.eleft + inPhoton.eright + inPhoton.etop + inPhoton.ebottom;
-        outPhoton.isEB = std::abs(outPhoton.scEta) < 1.4442;
-      
-        double chIsoS16EA(0.);
-        double nhIsoS16EA(0.);
-        double phIsoS16EA(0.);
-        double nhIsoS15EA(0.);
-        double phIsoS15EA(0.);
-        double absEta(std::abs(outPhoton.scEta));
-        if (absEta < 1.) {
-          nhIsoS15EA = 0.0599;
-          phIsoS15EA = 0.1271;
-          chIsoS16EA = 0.0360;
-          nhIsoS16EA = 0.0597;
-          phIsoS16EA = 0.1210;
-        }
-        else if (absEta < 1.479) {
-          nhIsoS15EA = 0.0819;
-          phIsoS15EA = 0.1101;
-          chIsoS16EA = 0.0377;
-          nhIsoS16EA = 0.0807;
-          phIsoS16EA = 0.1107;
-        }
-        else if (absEta < 2.) {
-          nhIsoS15EA = 0.0696;
-          phIsoS15EA = 0.0756;
-          chIsoS16EA = 0.0306;
-          nhIsoS16EA = 0.0629;
-          phIsoS16EA = 0.0699;
-        }
-        else if (absEta < 2.2) {
-          nhIsoS15EA = 0.0360;
-          phIsoS15EA = 0.1175;
-          chIsoS16EA = 0.0283;
-          nhIsoS16EA = 0.0197;
-          phIsoS16EA = 0.1056;
-        }
-        else if (absEta < 2.3) {
-          nhIsoS15EA = 0.0360;
-          phIsoS15EA = 0.1498;
-          chIsoS16EA = 0.0254;
-          nhIsoS16EA = 0.0184;
-          phIsoS16EA = 0.1457;
-        }
-        else if (absEta < 2.4) {
-          nhIsoS15EA = 0.0462;
-          phIsoS15EA = 0.1857;
-          chIsoS16EA = 0.0217;
-          nhIsoS16EA = 0.0284;
-          phIsoS16EA = 0.1719;
-        }
-        else {
-          nhIsoS15EA = 0.0656;
-          phIsoS15EA = 0.2183;
-          chIsoS16EA = 0.0167;
-          nhIsoS16EA = 0.0591;
-          phIsoS16EA = 0.1998;
-        }
-
-        outPhoton.chIsoS15 = inPhoton.chIso;
-        if (outPhoton.isEB) {
-          outPhoton.nhIsoS15 = inPhoton.nhIso + (0.014 - 0.0148) * inPhoton.pt() + (0.000019 - 0.000017) * inPhoton.pt() * inPhoton.pt();
-          outPhoton.phIsoS15 = inPhoton.phIso + (0.0053 - 0.0047) * inPhoton.pt();
-        }
-        else {
-          outPhoton.nhIsoS15 = inPhoton.nhIso + (0.0139 - 0.0163) * inPhoton.pt() + (0.000025 - 0.000014) * inPhoton.pt() * inPhoton.pt();
-          outPhoton.phIsoS15 = inPhoton.phIso + (0.0034 - 0.0034) * inPhoton.pt();
-        }
-
-        outPhoton.chIsoS15 += chIsoS16EA * _event.rho;
-        outPhoton.nhIsoS15 += (nhIsoS15EA - nhIsoS16EA) * _event.rho;
-        outPhoton.phIsoS15 += (phIsoS15EA - phIsoS16EA) * _event.rho;
-
-        // EA computed with iso/worstIsoEA.py
-        outPhoton.chIsoMax -= 0.094 * _event.rho;
-        if (outPhoton.chIsoMax < outPhoton.chIso)
-          outPhoton.chIsoMax = outPhoton.chIso;
-      }
-
-      return true;
+  panda::FileMerger::SkimFunction skim([this, &monophEvent](panda::EventBase& _event)->bool {
+      return this->skimFunction(static_cast<panda::Event&>(_event), monophEvent);
     });
 
-  merger_.setSkim(skim);
+  if (_doSkim) {
+    merger_.setOutEvent(&monophEvent);
+
+    merger_.setEventSelection("TMath::Abs(superClusters.eta) < 1.4442 && superClusters.rawPt > 150.");
+    merger_.setSkim(skim);
+  }
+  else {
+    // using this to merge photon skim - set input to EventMonophoton
+    merger_.setInEvent(&monophEvent);
+  }
 
   panda::utils::BranchList branchList = {
     "!pfCandidates",
@@ -161,7 +70,7 @@ PhotonSkimmer::run(char const* _outputPath, long _nEvents/* = -1*/, GoodLumiFilt
     "!neutralMet",
     "!photonMet",
     "!hfMet",
-    "!reoil",
+    "!recoil",
     "t1Met",
     "jets",
     "genJets",
@@ -171,4 +80,111 @@ PhotonSkimmer::run(char const* _outputPath, long _nEvents/* = -1*/, GoodLumiFilt
   merger_.selectBranches(branchList, true);
 
   merger_.merge(_outputPath, _nEvents);
+}
+
+bool
+PhotonSkimmer::skimFunction(panda::Event& _event, panda::EventMonophoton& _outEvent)
+{
+  if (goodlumi_ && !goodlumi_->isGoodLumi(_event.runNumber, _event.lumiNumber))
+    return false;
+
+  unsigned iPh(0);
+  for (; iPh != _event.photons.size(); ++iPh) {
+    auto& photon(_event.photons[iPh]);
+    if (std::abs(photon.superCluster->eta) < 1.4442 && photon.superCluster->rawPt > 150.)
+      break;
+  }
+  if (iPh == _event.photons.size())
+    return false;
+
+  // copy most of the event content (special operator= of EventMonophoton that takes Event as RHS)
+  _outEvent = _event;
+  
+  for (unsigned iPh(0); iPh != _event.photons.size(); ++iPh) {
+    auto& inPhoton(_event.photons[iPh]);
+    auto& superCluster(*inPhoton.superCluster);
+    auto& outPhoton(_outEvent.photons[iPh]);
+
+    outPhoton.scRawPt = superCluster.rawPt;
+    outPhoton.scEta = superCluster.eta;
+    outPhoton.e4 = inPhoton.eleft + inPhoton.eright + inPhoton.etop + inPhoton.ebottom;
+    outPhoton.isEB = std::abs(outPhoton.scEta) < 1.4442;
+      
+    double chIsoS16EA(0.);
+    double nhIsoS16EA(0.);
+    double phIsoS16EA(0.);
+    double nhIsoS15EA(0.);
+    double phIsoS15EA(0.);
+    double absEta(std::abs(outPhoton.scEta));
+    if (absEta < 1.) {
+      nhIsoS15EA = 0.0599;
+      phIsoS15EA = 0.1271;
+      chIsoS16EA = 0.0360;
+      nhIsoS16EA = 0.0597;
+      phIsoS16EA = 0.1210;
+    }
+    else if (absEta < 1.479) {
+      nhIsoS15EA = 0.0819;
+      phIsoS15EA = 0.1101;
+      chIsoS16EA = 0.0377;
+      nhIsoS16EA = 0.0807;
+      phIsoS16EA = 0.1107;
+    }
+    else if (absEta < 2.) {
+      nhIsoS15EA = 0.0696;
+      phIsoS15EA = 0.0756;
+      chIsoS16EA = 0.0306;
+      nhIsoS16EA = 0.0629;
+      phIsoS16EA = 0.0699;
+    }
+    else if (absEta < 2.2) {
+      nhIsoS15EA = 0.0360;
+      phIsoS15EA = 0.1175;
+      chIsoS16EA = 0.0283;
+      nhIsoS16EA = 0.0197;
+      phIsoS16EA = 0.1056;
+    }
+    else if (absEta < 2.3) {
+      nhIsoS15EA = 0.0360;
+      phIsoS15EA = 0.1498;
+      chIsoS16EA = 0.0254;
+      nhIsoS16EA = 0.0184;
+      phIsoS16EA = 0.1457;
+    }
+    else if (absEta < 2.4) {
+      nhIsoS15EA = 0.0462;
+      phIsoS15EA = 0.1857;
+      chIsoS16EA = 0.0217;
+      nhIsoS16EA = 0.0284;
+      phIsoS16EA = 0.1719;
+    }
+    else {
+      nhIsoS15EA = 0.0656;
+      phIsoS15EA = 0.2183;
+      chIsoS16EA = 0.0167;
+      nhIsoS16EA = 0.0591;
+      phIsoS16EA = 0.1998;
+    }
+
+    outPhoton.chIsoS15 = inPhoton.chIso;
+    if (outPhoton.isEB) {
+      outPhoton.nhIsoS15 = inPhoton.nhIso + (0.014 - 0.0148) * inPhoton.pt() + (0.000019 - 0.000017) * inPhoton.pt() * inPhoton.pt();
+      outPhoton.phIsoS15 = inPhoton.phIso + (0.0053 - 0.0047) * inPhoton.pt();
+    }
+    else {
+      outPhoton.nhIsoS15 = inPhoton.nhIso + (0.0139 - 0.0163) * inPhoton.pt() + (0.000025 - 0.000014) * inPhoton.pt() * inPhoton.pt();
+      outPhoton.phIsoS15 = inPhoton.phIso + (0.0034 - 0.0034) * inPhoton.pt();
+    }
+
+    outPhoton.chIsoS15 += chIsoS16EA * _event.rho;
+    outPhoton.nhIsoS15 += (nhIsoS15EA - nhIsoS16EA) * _event.rho;
+    outPhoton.phIsoS15 += (phIsoS15EA - phIsoS16EA) * _event.rho;
+
+    // EA computed with iso/worstIsoEA.py
+    outPhoton.chIsoMax -= 0.094 * _event.rho;
+    if (outPhoton.chIsoMax < outPhoton.chIso)
+      outPhoton.chIsoMax = outPhoton.chIso;
+  }
+
+  return true;
 }
--- monophoton/misc/photonskim.py
+++ monophoton/misc/photonskim.py
@@ -8,19 +8,22 @@ import collections
 import time
 from argparse import ArgumentParser
 
+NENTRIES = -1
+
+padd = os.environ['CMSSW_BASE'] + '/bin/' + os.environ['SCRAM_ARCH'] + '/padd'
+condor_run = '/home/yiiyama/bin/condor-run'
+
 argParser = ArgumentParser(description = 'Skim and slim the primary panda files into monophoton format.')
 argParser.add_argument('sname', help = 'Sample name.')
 argParser.add_argument('--json', '-j', metavar = 'PATH', dest = 'json', default = '', help = 'Good lumi list to apply.')
 argParser.add_argument('--catalog', '-c', metavar = 'PATH', dest = 'catalog', default = '/home/cmsprod/catalog/t2mit', help = 'Source file catalog.')
 argParser.add_argument('--filesets', '-f', metavar = 'ID', dest = 'filesets', nargs = '+', default = [], help = 'Fileset id (empty => all filesets).')
-argParser.add_argument('--batch', '-B', action = 'store_true', dest = 'batch', help = 'Use condor-run to run the skim in parallel.')
+argParser.add_argument('--split', '-B', action = 'store_true', dest = 'split', help = 'Use condor-run to run the skim in parallel.')
 argParser.add_argument('--merge', '-M', action = 'store_true', dest = 'merge', help = 'Run padd at the end of execution to produce a single output.')
 
 args = argParser.parse_args()
 argv = list(sys.argv)
 sys.argv = []
-
-NENTRIES = -1
 
 thisdir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(thisdir)
@@ -38,15 +41,21 @@ ROOT.gSystem.AddIncludePath('-I' + config.dataformats)
 ROOT.gSystem.AddIncludePath('-I' + monoxdir + '/common')
 
 ROOT.gROOT.LoadMacro(thisdir + '/PhotonSkim.cc+')
+ROOT.PhotonSkimmer # force crash if compilation fails
 
 sample = allsamples[args.sname]
 
+splitOutDir = config.photonSkimDir + '/' + args.sname
+
+if len(args.filesets) != 0:
+    outDir = splitOutDir
+else:
+    outDir = config.photonSkimDir
+
 try:
-    os.makedirs(config.photonSkimDir)
+    os.makedirs(outDir)
 except OSError:
     pass
-
-outnameBase = config.photonSkimDir + '/' + args.sname
 
 cdir = args.catalog + '/' + sample.book + '/' + sample.fullname
 
@@ -56,9 +65,9 @@ with open(cdir + '/Filesets') as filesetList:
         fileset, xrdpath = line.split()[:2]
         directories[fileset] = xrdpath.replace('root://xrootd.cmsaf.mit.edu/', '/mnt/hadoop/cms')
 
-if args.batch:
+if args.split:
     commonArgs = list(argv[1:])
-    for a in ['-B', '--batch', '-M', '--merge']:
+    for a in ['-B', '--split', '-M', '--merge']:
         if a in commonArgs:
             commonArgs.remove(a)
 
@@ -67,7 +76,15 @@ if args.batch:
     else:
         filesets = list(args.filesets)
 
-    proc = subprocess.Popen(['/home/yiiyama/bin/condor-run', os.path.realpath(__file__), '-H', '-e', ' '.join(commonArgs), '-j'] + ['-f %s' % fileset for fileset in filesets], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    # remove the output first
+    for fileset in filesets:
+        path = splitOutDir + '/' + args.sname + '_' + fileset + '.root'
+        try:
+            os.remove(path)
+        except:
+            pass
+
+    proc = subprocess.Popen([condor_run, os.path.realpath(__file__), '-H', '-e', ' '.join(commonArgs), '-j'] + ['-f %s' % fileset for fileset in filesets], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     out, err = proc.communicate()
     print out.strip()
     print err.strip()
@@ -76,7 +93,8 @@ if args.batch:
 
     while True:
         for fileset in filesets:
-            if not os.path.exists(outnameBase + '_' + fileset + '.root'):
+            path = splitOutDir + '/' + args.sname + '_' + fileset + '.root'
+            if not os.path.exists(path) or os.stat(path).st_size == 0:
                 break
 
         else: # all files exist
@@ -85,10 +103,6 @@ if args.batch:
         time.sleep(10)
 
 else:
-    goodlumi = None
-    if args.json:
-        goodlumi = makeGoodLumiFilter(args.json)
-    
     fullpaths = collections.defaultdict(list)
     
     with open(cdir + '/Files') as fileList:
@@ -103,39 +117,42 @@ else:
             fullpaths[fileset].append(fullpath)
     
     if len(args.filesets) == 0:
-        skimmer = ROOT.PhotonSkimmer()
+        allpaths = {'': sum(paths for paths in fullpaths.values())}
+    else:
+        allpaths = {}
         for fileset, paths in fullpaths.items():
-            for path in paths:
-                skimmer.addSourcePath(path)
+            if fileset in args.filesets:
+                allpaths['_%s' % fileset] = paths
 
-        outputName = args.sname + '.root'
-    
-        skimmer.run('/tmp/' + args.sname + '.root', NENTRIES, goodlumi)
-    
-        shutil.copyfile('/tmp/' + outputName, config.photonSkimDir + '/' + outputName)
+    for suffix, paths in allpaths.items():
+        skimmer = ROOT.PhotonSkimmer()
+        for path in paths:
+            skimmer.addSourcePath(path)
+
+        if args.json:
+            skimmer.setLumiFilter(makeGoodLumiFilter(args.json))
+
+        outputName = args.sname + suffix + '.root'
+
+        skimmer.run('/tmp/' + outputName, True, NENTRIES)
+
+        # shutil.move fails if the destination exists already
+        shutil.copy('/tmp/' + outputName, outDir)
         os.remove('/tmp/' + outputName)
     
-    else:
-        filesets = list(args.filesets)
-    
-        for fileset in filesets:
-            skimmer = ROOT.PhotonSkimmer()
-    
-            for path in fullpaths[fileset]:
-                skimmer.addSourcePath(path)
-    
-            outputName = args.sname + '_' + fileset + '.root'
-    
-            skimmer.run('/tmp/' + outputName, NENTRIES, goodlumi)
-    
-            shutil.copyfile('/tmp/' + outputName, config.photonSkimDir + '/' + outputName)
-            os.remove('/tmp/' + outputName)
-
 if args.merge:
-    proc = subprocess.Popen([os.environ['CMSSW_BASE'] + '/bin/' + os.environ['SCRAM_ARCH'] + '/padd', '/tmp/' + args.sname + '.root'] + [outnameBase + '_' + fileset + '.root' for fileset in filesets], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-    out, err = proc.communicate()
-    print out.strip()
-    print err.strip()
+    if len(args.filesets) == 0:
+        filesets = directories.keys()
+    else:
+        filesets = args.filesets
 
-    shutil.copyfile('/tmp/' + args.sname + '.root', outnameBase + '.root')
-    os.remove('/tmp/' + args.sname + '.root')
+    outputName = args.sname + '.root'
+
+    skimmer = ROOT.PhotonSkimmer()
+    for fileset in filesets:
+        skimmer.addSourcePath(splitOutDir + '/' + args.sname + '_' + fileset + '.root')
+
+    skimmer.run('/tmp/' + outputName, False)
+
+    shutil.copy('/tmp/' + outputName, config.photonSkimDir)
+    os.remove('/tmp/' + outputName)
