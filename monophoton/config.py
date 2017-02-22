59df41d798d98ccbf364433268ed90a98e161073
Brandon <ballen@mit.edu>
2017-02-22 08:50:15 -0500

    making photon skims myself.

--- monophoton/config.py
+++ monophoton/config.py
@@ -5,7 +5,7 @@ skimDir = '/data/t3home000/' + os.environ['USER'] + '/studies/test/skim'
 histDir = '/data/t3home000/' + os.environ['USER'] + '/studies/test'
 ntuplesDir = '/mnt/hadoop/scratch/yiiyama/panda'
 dataNtuplesDir = ntuplesDir
-photonSkimDir = '/data/t3home000/yiiyama/studies/test/photonskim'
+photonSkimDir = '/data/t3home000/' + os.environ['USER'] + '/studies/test/photonskim'
 
 libobjs = 'libPandaTreeObjects.so'
 libutils = 'libPandaTreeUtils.so'
