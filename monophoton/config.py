35229a2d3c2660ab960a1c443fe20b05524f646e
Yutaro Iiyama <yiiyama@mit.edu>
2017-05-27 00:16:38 -0400

    printlevel isn't really read from config

--- monophoton/config.py
+++ monophoton/config.py
@@ -1,14 +1,13 @@
-import logging
 import os
 
 # tier3 set up
 skimDir = '/mnt/hadoop/scratch/' + os.environ['USER'] + '/monophoton/skim'
 histDir = '/data/t3home000/' + os.environ['USER'] + '/studies/monophoton_panda'
 ntuplesDir = '/mnt/hadoop/cms/store/user/paus'
+#ntuplesDir = '/mnt/hadoop/scratch/yiiyama'
 dataNtuplesDir = ntuplesDir
 
+#libobjs = os.environ['CMSSW_BASE'] + '/src/PandaTree/lib/libpanda.so'
 libobjs = 'libPandaTreeObjects.so'
 libutils = 'libPandaTreeUtils.so'
 dataformats = os.environ['CMSSW_BASE'] + '/src/PandaTree'
-
-printLevel = logging.INFO
