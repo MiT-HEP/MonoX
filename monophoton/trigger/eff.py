66f0331fa59644ad559d1a2e692db42a9568fa6e
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-06 14:22:17 -0400

    using main skim now

--- monophoton/trigger/eff.py
+++ monophoton/trigger/eff.py
@@ -1,6 +1,5 @@
 # Trigger efficiency measurement using X+photon events
 #   eff.py object sname vname tname [output name]
-# can use wildcard for any of the three
 
 import os
 import sys
@@ -9,9 +8,6 @@ import array
 import ROOT
 ROOT.gROOT.SetBatch(True)
 
-# ROOT.gSystem.Load('libRooFit.so')
-# ROOT.gSystem.Load('/home/yiiyama/cms/studies/fittools/libFitTools.so')
-
 thisdir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(thisdir)
 sys.path.append(basedir)
@@ -19,214 +15,249 @@ from datasets import allsamples
 from plotstyle import SimpleCanvas
 import config
 
-oname = sys.argv[1]
-sname = sys.argv[2]
-vname = sys.argv[3]
-tname = sys.argv[4]
-try:
-    outname = sys.argv[5]
-except IndexError:
-    outname = ''
-
 ROOT.gStyle.SetNdivisions(510, 'X')
 
-samplesConf = {
-    'sph': ['sph-*'],
-    'sel': ['sel-16*'],
-    'smu': ['smu-*']
-}
+FITEFFICIENCY = False
 
-snames = samplesConf[sname]
+measurements = [
+    ('photon', 'sph', 'sph-16*', 'tpeg', 'probes.medium'),
+    ('photon', 'sel', 'sel-16*', 'tpeg', 'probes.medium && !probes.pixelVeto'),
+    ('electron', 'sel', 'sel-16*', 'tpegLowPt', 'probes.tight'),
+    ('muon', 'smu', 'smu-16*', 'tpmgLowPt', 'probes.tight')
+]
 
 ## SETUP
 
-workDir = config.histDir + '/trigger_gsfix'
-plotDir = 'trigger_gsfix'
+outName = 'trigger'
+outFileName = config.histDir + '/trigger/' + outName + '.root'
+outputFile = ROOT.TFile.Open(outFileName, 'recreate')
 
-if outname:
-    outputFile = ROOT.TFile.Open(outname, 'recreate')
+vconfs = {}
+tconfs = {}
 
-if oname == 'photon':
-    if sname == 'sel':
-        baseline = '!probe.pixelVeto'
-    else:
-        baseline = '1'
-
-    vconf = {
-        'pt': ('p_{T}^{#gamma} (GeV)', 'probe.pt', baseline, array.array('d', [30. + 5. * x for x in range(14)] + [100. + 10. * x for x in range(10)] + [200. + 20. * x for x in range(5)] + [300. + 50. * x for x in range(10)])),
-        'ptzoom': ('p_{T}^{#gamma} (GeV)', 'probe.pt', baseline, array.array('d', [30. + 2. * x for x in range(85)] + [200. + 10. * x for x in range(10)])),
-        'hOverE': ('H/E', 'probe.hOverE', baseline + ' && probe.pt > 175.', (25, 0., 0.05)),
-        'hcalE': ('E^{HCAL} (GeV)', 'probe.pt * TMath::CosH(probe.eta) * probe.hOverE', baseline + ' && probe.pt > 175.', (25, 0., 5)),
-        'run': ('Run', 'run', baseline + ' && probe.pt > 175.', (26, 271050., 284050.))
-    }
-
-    tconf = {
-        'sph165': ('probe.matchHLT[2]', 'probe.matchL1[2]', 'HLT/L1'),
-        'l1all': ('probe.matchL1[2]', '1', 'L1 seed'),
-        'l1eg40': ('probe.matchL1[5]', '1', 'L1 seed'),
-        'sph165abs': ('probe.matchHLT[2]', '1', 'L1&HLT')
-    }
-
-elif oname == 'muon':
-    vconf = {
-        'pt': ('p_{T}^{#gamma} (GeV)', 'probe.pt', '1', (50, 0., 50.)),
-        'ptzoom': ('p_{T}^{#gamma} (GeV)', 'probe.pt', '1', array.array('d', [30. + 2. * x for x in range(85)] + [200. + 10. * x for x in range(10)])),
-        'hOverE': ('H/E', 'probe.hOverE', 'probe.pt > 200.', (25, 0., 0.05)),
-        'hcalE': ('E^{HCAL} (GeV)', 'probe.pt * TMath::CosH(probe.eta) * probe.hOverE', 'probe.pt > 200.', (25, 0., 5)),
-        'run': ('Run', 'run', 'probe.pt > 200.', (350, 271000., 274500.)),
-        'leppt': ('p_{T}^{#gamma} (GeV)', 'probe.pt', '1', array.array('d', [0. + 5. * x for x in range(10)] + [50. + 10. * x for x in range(6)])),
-    }
-
-    tconf = {
-        'mu20ortrk20': ('probe.matchHLT[0] || probe.matchHLT[1]', '1', ''),
-    }
-
-elif oname == 'electron':
-    vconf = {
-        'pt': ('p_{T}^{#gamma} (GeV)', 'probe.pt', '1', (50, 0., 50.)),
-        'ptzoom': ('p_{T}^{#gamma} (GeV)', 'probe.pt', '1', array.array('d', [30. + 2. * x for x in range(85)] + [200. + 10. * x for x in range(10)])),
-        'hOverE': ('H/E', 'probe.hOverE', 'probe.pt > 200.', (25, 0., 0.05)),
-        'hcalE': ('E^{HCAL} (GeV)', 'probe.pt * TMath::CosH(probe.eta) * probe.hOverE', 'probe.pt > 200.', (25, 0., 5)),
-        'run': ('Run', 'run', 'probe.pt > 200.', (350, 271000., 274500.)),
-        'leppt': ('p_{T}^{#gamma} (GeV)', 'probe.pt', '1', array.array('d', [0. + 5. * x for x in range(10)] + [50. + 10. * x for x in range(6)])),
-    }
-
-    tconf = {
-        'el27': ('probe.matchHLT[1]', '1', ''),
-    }
-
-vtitle, vexpr, baseline, binning = vconf[vname]
-
-passdef, denomdef, title = tconf[tname]
-
-if sname == 'jht' and vname == 'pt' and (tname == 'l1all' or tname == 'l1egallsph165'):
-    binning = array.array('d', [30. + 5. * x for x in range(14)] + [100. + 10. * x for x in range(10)] + [200. + 20. * x for x in range(5)] + [300. + 50. * x for x in range(14)])
-
-## SOURCE
-
-tree = ROOT.TChain('triggerTree')
-lumi = 0.
-for sample in allsamples.getmany(snames):
-    lumi += sample.lumi
-    print 'adding', workDir + '/' + sample.name + '_' + oname + '.root'
-    tree.Add(workDir + '/' + sample.name + '_' + oname + '.root')
-
-canvas = SimpleCanvas(lumi = lumi)
-canvas.legend.setPosition(0.7, 0.3, 0.9, 0.5)
+vconfs['photon'] = {
+    'pt': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', [30. + 5. * x for x in range(14)] + [100. + 10. * x for x in range(10)] + [200. + 20. * x for x in range(5)] + [300. + 50. * x for x in range(10)]),
+    'ptzoom': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', [30. + 2. * x for x in range(85)] + [200. + 10. * x for x in range(10)]),
+    'hOverE': ('H/E', 'probes.hOverE', 'probes.pt_ > 175.', (25, 0., 0.05)),
+    'hcalE': ('E^{HCAL} (GeV)', 'probes.pt_ * TMath::CosH(probes.eta_) * probes.hOverE', 'probes.pt_ > 175.', (25, 0., 5)),
+    'run': ('Run', 'run', 'probes.pt_ > 175.', (26, 271050., 284050.))
+}
+vconfs['electron'] = {
+    'pt': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', (50, 0., 50.)),
+    'ptzoom': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', [30. + 2. * x for x in range(85)] + [200. + 10. * x for x in range(10)]),
+    'hOverE': ('H/E', 'probes.hOverE', 'probes.pt_ > 200.', (25, 0., 0.05)),
+    'hcalE': ('E^{HCAL} (GeV)', 'probes.pt_ * TMath::CosH(probes.eta_) * probes.hOverE', 'probes.pt_ > 200.', (25, 0., 5)),
+    'run': ('Run', 'run', 'probes.pt_ > 200.', (350, 271000., 274500.)),
+    'leppt': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', [0. + 5. * x for x in range(10)] + [50. + 10. * x for x in range(6)]),
+}
+vconfs['muon'] = {
+    'pt': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', (50, 0., 50.)),
+    'ptzoom': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', [30. + 2. * x for x in range(85)] + [200. + 10. * x for x in range(10)]),
+    'hOverE': ('H/E', 'probes.hOverE', 'probes.pt_ > 200.', (25, 0., 0.05)),
+    'hcalE': ('E^{HCAL} (GeV)', 'probes.pt_ * TMath::CosH(probes.eta_) * probes.hOverE', 'probes.pt_ > 200.', (25, 0., 5)),
+    'run': ('Run', 'run', 'probes.pt_ > 200.', (350, 271000., 274500.)),
+    'leppt': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', [0. + 5. * x for x in range(10)] + [50. + 10. * x for x in range(6)]),
+}
 
 # matchL1[2] -> SEG34IorSEG40IorSJet200
 # matchHLT[2] -> Ph165HE10
 
+Photon = ROOT.panda.Photon
+Electron = ROOT.panda.Electron
+Muon = ROOT.panda.Muon
+
+tconfs['photon'] = {
+    'sph165': ('probes.triggerMatch[%d]' % Photon.fPh165HE10, 'probes.triggerMatch[%d]' % Photon.fSEG34IorSEG40IorSJet200, 'HLT/L1'),
+    'l1all': ('probes.matchL1[%d]' % Photon.fSEG34IorSEG40IorSJet200, '', 'L1 seed'),
+    'l1eg40': ('probes.matchL1[%d]' % Photon.fSEG34IorSEG40, '', 'L1 seed'),
+    'sph165abs': ('probes.triggerMatch[%d]' % Photon.fPh165HE10, '', 'L1&HLT')
+}
+tconfs['electron'] = {
+    'el27': ('probes.triggerMatch[%d]' % Electron.fEl27Tight, '', 'HLT'),
+}
+tconfs['muon'] = {
+    'mu24ortrk24': ('probes.triggerMatch[%d] || probes.triggerMatch[%d]' % (Muon.fIsoMu24, Muon.fIsoTkMu24), '', 'HLT'),
+}
+
+# TTree output for fitting
+vtconfs = {}
+vtconfs['photon'] = []
+vtconfs['electron'] = [
+    ('ptzoom', 'el27')
+]
+vtconfs['muon'] = []
+
+
+## FILL DISTRIBUTIONS AND GRAPHS
+
+for oname, mname, snames, region, probeSel in measurements:
+    measDir = outputFile.mkdir(oname + '_' + mname)
+
+    baseSelection = 'tp.mass > 60. && tp.mass < 120. && ' + probeSel
+
+    # make empty histograms for all (variable, trigger) combination
+    histograms = []
+
+    for vname, (vtitle, vexpr, baseline, binning) in vconfs[oname].items():
+        outDir = measDir.mkdir(vname)
+
+        if type(binning) is tuple:
+            template = ROOT.TH1D('template', ';' + vtitle, *binning)
+        else:
+            template = ROOT.TH1D('template', ';' + vtitle, len(binning) - 1, array.array('d', binning))
+
+        for tname, (passdef, denomdef, title) in tconfs[oname].items():
+            outDir.cd()
+            hpass = template.Clone(tname + '_pass')
+            hbase = template.Clone(tname + '_base')
+            histograms.extend([hpass, hbase])
+
+    # fill the histograms sample by sample
+    for sample in allsamples.getmany(snames):
+        sourceName = config.skimDir + '/' + sample.name + '_' + region + '.root'
+
+        plotter = ROOT.Plotter(sourceName)
+        plotter.setBaseSelection(baseSelection)
+
+        for vname, (vtitle, vexpr, baseline, binning) in vconfs[oname].items():
+            outDir = measDir.GetDirectory(vname)
+
+            for tname, (passdef, denomdef, title) in tconfs[oname].items():
+                hpass = outDir.Get(tname + '_pass')
+                hbase = outDir.Get(tname + '_base')
+
+                sel = passdef
+                if baseline:
+                    sel += ' && ' + baseline
+                if denomdef:
+                    sel += ' && ' + denomdef
+
+                plotter.addPlot(hpass, vexpr, sel, True)
+
+                sel = ''
+                if baseline:
+                    sel += ' && ' + baseline
+                if denomdef:
+                    sel += ' && ' + denomdef
+
+                plotter.addPlot(hbase, vexpr, sel, True)
+
+            template.Delete()
+
+        plotter.fillPlots()
+
+    # make efficiency graphs and save
+    for vname in vconfs[oname]:
+        for tname in tconfs[oname]:
+            hpass = measDir.Get(vname + '/' + tname + '_pass')
+            hbase = measDir.Get(vname + '/' + tname + '_base')
+            eff = ROOT.TGraphAsymmErrors(hpass, hbase)
+            measDir.GetDirectory(vname).cd()
+            eff.Write(tname + '_eff')
+
+    # save the distributions
+    for hist in histograms:
+        hist.GetDirectory().cd()
+        hist.Write()
+
+## PLOT GRAPHS
+
 work = ROOT.RooWorkspace('work', 'work')
 xvar = work.factory('x[-6500.,6500.]')
 
-# fitter = ROOT.EfficiencyFitter.singleton()
-tmpfile = ROOT.TFile.Open('/tmp/trigeff_tmp.root', 'recreate')
+if FITEFFICIENCY:
+    ROOT.gSystem.Load('libRooFit.so')
+    ROOT.gSystem.Load('/home/yiiyama/cms/studies/fittools/libFitTools.so')
+    fitter = ROOT.EfficiencyFitter.singleton()
 
-canvas.Clear()
+canvas = SimpleCanvas(lumi = lumi)
+canvas.legend.setPosition(0.7, 0.3, 0.9, 0.5)
 
-if type(binning) is tuple:
-    passing = ROOT.TH1D('passing', ';' + vtitle, *binning)
-    denom = ROOT.TH1D('denom', ';' + vtitle, *binning)
-else:
-    passing = ROOT.TH1D('passing', ';' + vtitle, len(binning) - 1, binning)
-    denom = ROOT.TH1D('denom', ';' + vtitle, len(binning) - 1, binning)
+for oname, mname, snames, region, probeSel in measurements:
+    measDir = outputFile.GetDirectory(oname + '_' + mname)
 
-print vexpr, '(%s) && (%s)' % (baseline, denomdef)
+    for vname, (vtitle, vexpr, baseline, binning) in vconfs[oname].items():
+        outDir = measDir.GetDirectory(vname)
 
-tree.Draw(vexpr + '>>denom', '(%s) && (%s)' % (baseline, denomdef))
-tree.Draw(vexpr + '>>passing', '(%s) && (%s) && (%s)' % (baseline, denomdef, passdef))
+        for tname, (passdef, denomdef, title) in tconfs[oname].items():
+            eff = outDir.Get(tname + '_eff')
 
-print passing.GetEntries(), denom.GetEntries()
+            canvas.Clear()
+            canvas.SetGrid()
+            
+            canvas.legend.Clear()
+            if title:
+                canvas.legend.add('eff', title = title, opt = 'LP', color = ROOT.kBlack, mstyle = 8)
+                canvas.legend.apply('eff', eff)
+            else:
+                eff.SetMarkerColor(ROOT.kBlack)
+                eff.SetMarkerStyle(8)
+            
+            canvas.addHistogram(eff, drawOpt = 'EP')
+            
+            canvas.xtitle = vtitle
+            canvas.ylimits = (0., 1.2)
+            
+            canvas.Update()
+            
+            if type(binning) is tuple:
+                canvas.addLine(binning[0], 1., binning[2], 1.)
+                eff.GetXaxis().SetLimits(binning[1], binning[2])
+            else:
+                canvas.addLine(binning[0], 1., binning[-1], 1.)
+                eff.GetXaxis().SetLimits(binning[0], binning[-1])
+            
+            eff.GetYaxis().SetRangeUser(0., 1.2)
+            
+            canvas.printWeb(outName, oname + '_' + mname + '_' + vname + '_' + tname, logy = False)
 
-eff = ROOT.TGraphAsymmErrors(passing, denom)
-
-if outname:
-    outputFile.cd()
-    eff.Write(vname + '_' + tname + '_' + sname)
-
-canvas.SetGrid()
-
-canvas.legend.Clear()
-if title:
-    canvas.legend.add('eff', title = title, opt = 'LP', color = ROOT.kBlack, mstyle = 8)
-    canvas.legend.apply('eff', eff)
-else:
-    eff.SetMarkerColor(ROOT.kBlack)
-    eff.SetMarkerStyle(8)
-
-canvas.addHistogram(eff, drawOpt = 'EP')
-
-canvas.xtitle = vtitle
-canvas.ylimits = (0., 1.2)
-
-canvas.Update()
-
-if type(binning) is tuple:
-    canvas.addLine(binning[0], 1., binning[2], 1.)
-    eff.GetXaxis().SetLimits(binning[1], binning[2])
-else:
-    canvas.addLine(binning[0], 1., binning[-1], 1.)
-    eff.GetXaxis().SetLimits(binning[0], binning[-1])
-
-eff.GetYaxis().SetRangeUser(0., 1.2)
-
-canvas.printWeb(plotDir, vname + '_' + tname + '_' + sname + '_' + oname, logy = False)
-
-func = None
-marker = None
-
-if vname == 'pt' and (tname == 'l1' or tname == 'l1hlt') and sname == 'jht':
-    func = work.factory('PolyVar::func(x, {intercept[1.04,0.,2.], slope[-0.001,-0.01,0.]})')
-    params = ROOT.RooArgList(work.arg('intercept'), work.arg('slope'))
-    fitmin = 300.
-    fitmax = 1000.
-    
-elif vname == 'ptzoom' and tname == 'hlt' and sname == 'sel':
-    func = work.factory('FormulaVar::func("(@0 < @1) * @2 * (1. + TMath::Erf((@3 - @2) * @4 / @2 * (@0 - @1))) + (@0 > @1) * (@2 + (@3 - @2) * TMath::Erf(@4 * (@0 - @1)))", {x, turn[170.,0.,300.], norm1[0.5,0.,1.], norm[0.9,0.,1.], rise2[0.1,0.,1.]})')
-    params = ROOT.RooArgList(work.arg('turn'), work.arg('norm1'), work.arg('norm'), work.arg('rise2'))
-    fitmin = 150.
-    fitmax = 250.
-
-    marker = 175.
-
-if func:
-    tmpfile.cd()
-    reduced = tree.CopyTree('({baseline}) && ({denomdef})'.format(baseline = baseline, denomdef = denomdef))
-
-    xvar.setRange(fitmin, fitmax)
-    
-    fitter.setXRange(fitmin, fitmax)
-    fitter.setTarget(reduced, passdef, vexpr)
-    fitter.setLikelihood(func, params, xvar)
-
-    fitter.fit()
-
-    tf1 = func.asTF(ROOT.RooArgList(xvar))
-    tf1.SetNpx(1000)
-
-    canvas.Clear()
-    canvas.legend.add('fit', title = 'Fit', opt = 'L', color = ROOT.kRed, lwidth = 2)
-    canvas.legend.apply('fit', tf1)
-
-    canvas.SetGrid()
-
-    canvas.addHistogram(eff, drawOpt = 'EP')
-    canvas.addHistogram(tf1, drawOpt = '')
-    
-    if type(binning) is tuple:
-        canvas.addLine(binning[1], 1., binning[2], 1.)
-        eff.GetXaxis().SetLimits(binning[1], binning[2])
-        tf1.SetRange(binning[1], binning[2])
-    else:
-        canvas.addLine(binning[0], 1., binning[-1], 1.)
-        eff.GetXaxis().SetLimits(binning[0], binning[-1])
-        tf1.SetRange(binning[0], binning[-1])
-    
-    canvas.xtitle = vtitle
-    canvas.ylimits = (0., 1.2)
-
-    if marker is not None:
-        canvas.addLine(marker, 0., marker, 1.2, color = ROOT.kRed)
-    
-    canvas.printWeb('trigger', vname + '_' + tname + '_' + sname + '_fit', logy = False)
+# TO BE FIXED
+#    func = None
+#    marker = 0.
+#    
+#    if (vname, tname) in vtconfs[oname]:
+#        func = work.factory('FormulaVar::func("(@0 < @1) * @2 * (1. + TMath::Erf((@3 - @2) * @4 / @2 * (@0 - @1))) + (@0 > @1) * (@2 + (@3 - @2) * TMath::Erf(@4 * (@0 - @1)))", {x, turn[170.,0.,300.], norm1[0.5,0.,1.], norm[0.9,0.,1.], rise2[0.1,0.,1.]})')
+#        params = ROOT.RooArgList(work.arg('turn'), work.arg('norm1'), work.arg('norm'), work.arg('rise2'))
+#        fitmin = 150.
+#        fitmax = 250.
+#    
+#        marker = 175.
+#    
+#        reduced = tree.CopyTree('({baseline}) && ({denomdef})'.format(baseline = baseline, denomdef = denomdef))
+#    
+#        xvar.setRange(fitmin, fitmax)
+#        
+#        fitter.setXRange(fitmin, fitmax)
+#        fitter.setTarget(reduced, passdef, vexpr)
+#        fitter.setLikelihood(func, params, xvar)
+#    
+#        fitter.fit()
+#    
+#        tf1 = func.asTF(ROOT.RooArgList(xvar))
+#        tf1.SetNpx(1000)
+#    
+#        canvas.Clear()
+#        canvas.legend.add('fit', title = 'Fit', opt = 'L', color = ROOT.kRed, lwidth = 2)
+#        canvas.legend.apply('fit', tf1)
+#    
+#        canvas.SetGrid()
+#    
+#        canvas.addHistogram(eff, drawOpt = 'EP')
+#        canvas.addHistogram(tf1, drawOpt = '')
+#        
+#        if type(binning) is tuple:
+#            canvas.addLine(binning[1], 1., binning[2], 1.)
+#            eff.GetXaxis().SetLimits(binning[1], binning[2])
+#            tf1.SetRange(binning[1], binning[2])
+#        else:
+#            canvas.addLine(binning[0], 1., binning[-1], 1.)
+#            eff.GetXaxis().SetLimits(binning[0], binning[-1])
+#            tf1.SetRange(binning[0], binning[-1])
+#        
+#        canvas.xtitle = vtitle
+#        canvas.ylimits = (0., 1.2)
+#    
+#        if marker > 0.:
+#            canvas.addLine(marker, 0., marker, 1.2, color = ROOT.kRed)
+#        
+#        canvas.printWeb('trigger', vname + '_' + tname + '_' + sname + '_fit', logy = False)
