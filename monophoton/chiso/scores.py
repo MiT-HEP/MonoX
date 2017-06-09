92ffef72e34dc4ff915fe7e257ea305881927c4b
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-08 22:29:33 -0400

    production version

--- monophoton/chiso/scores.py
+++ monophoton/chiso/scores.py
@@ -15,11 +15,7 @@ import config
 
 import ROOT
 
-canvas = SimpleCanvas()
-canvas.legend.setPosition(0.7, 0.7, 0.9, 0.9)
-canvas.legend.add('low', '175 < p_{T} < 200 GeV', lcolor = ROOT.kRed, lwidth = 2, opt = 'L')
-canvas.legend.add('med', '200 < p_{T} < 250 GeV', lcolor = ROOT.kBlue, lwidth = 2, opt = 'L')
-canvas.legend.add('high', 'p_{T} > 250 GeV', lcolor = ROOT.kGreen, lwidth = 2, opt = 'L')
+chIsoCut = 1.163
 
 tree = ROOT.TChain('events')
 tree.Add(config.skimDir + '/sph-16*_dimuAllPhoton.root')
@@ -28,25 +24,35 @@ tree.Add(config.skimDir + '/sph-16*_monomuAllPhoton.root')
 
 outputFile = ROOT.TFile.Open(basedir + '/data/vertex_scores.root', 'recreate')
 
-low = ROOT.TH1D('low', ';ln(score / GeV^{2})', 20, 0., 15.)
-med = ROOT.TH1D('med', ';ln(score / GeV^{2})', 20, 0., 15.)
-high = ROOT.TH1D('high', ';ln(score / GeV^{2})', 20, 0., 15.)
+iso = ROOT.TH2D('iso', ';p_{T}^{jet} (GeV);ln(score / GeV^{2});', 3, array.array('d', [175., 200., 250., 6500.]), 20, 0., 15.)
+noIso = ROOT.TH2D('noIso', ';p_{T}^{jet} (GeV);ln(score / GeV^{2});', 3, array.array('d', [175., 200., 250., 6500.]), 20, 0., 15.)
 
-tree.Draw('TMath::Log(lvertex.score)>>low', 'Sum$(photons.sieie > 0.012 && photons.chIso < 1. && photons.scRawPt < 200.) != 0')
-tree.Draw('TMath::Log(lvertex.score)>>med', 'Sum$(photons.sieie > 0.012 && photons.chIso < 1. && photons.scRawPt > 200. && photons.scRawPt < 250.) != 0')
-tree.Draw('TMath::Log(lvertex.score)>>high', 'Sum$(photons.sieie > 0.012 && photons.chIso < 1. && photons.scRawPt > 250.) != 0')
+tree.Draw('TMath::Log(lvertex.score):photons.scRawPt>>iso', 'photons.sieie > 0.012 && photons.chIso < %f' % chIsoCut, 'goff')
+tree.Draw('TMath::Log(lvertex.score):photons.scRawPt>>noIso', 'photons.sieie > 0.012', 'goff')
 
-low.Scale(1. / low.GetSumOfWeights())
-med.Scale(1. / med.GetSumOfWeights())
-high.Scale(1. / high.GetSumOfWeights())
+for hist in [iso, noIso]:
+    for iX in range(1, hist.GetNbinsX() + 1):
+        sliceTotal = 0.
+        for iY in range(1, hist.GetNbinsY() + 1):
+            sliceTotal += hist.GetBinContent(iX, iY)
+    
+        for iY in range(1, hist.GetNbinsY() + 1):
+            hist.SetBinContent(iX, iY, hist.GetBinContent(iX, iY) / sliceTotal)
+
 
 outputFile.cd()
 outputFile.Write()
 
-canvas.addHistogram(low)
-canvas.addHistogram(med)
-canvas.addHistogram(high)
+#canvas = SimpleCanvas()
+#canvas.legend.setPosition(0.7, 0.7, 0.9, 0.9)
+#canvas.legend.add('low', '175 < p_{T} < 200 GeV', lcolor = ROOT.kRed, lwidth = 2, opt = 'L')
+#canvas.legend.add('med', '200 < p_{T} < 250 GeV', lcolor = ROOT.kBlue, lwidth = 2, opt = 'L')
+#canvas.legend.add('high', 'p_{T} > 250 GeV', lcolor = ROOT.kGreen, lwidth = 2, opt = 'L')
 
-canvas.applyStyles()
-
-canvas.printWeb('chiso', 'scores_by_pt', logy = False)
+#canvas.addHistogram(low)
+#canvas.addHistogram(med)
+#canvas.addHistogram(high)
+#
+#canvas.applyStyles()
+#
+#canvas.printWeb('chiso', 'scores_by_pt', logy = False)
