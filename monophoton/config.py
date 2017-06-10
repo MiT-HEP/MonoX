ecb4718ccbe18b4554026ba69efcc0b6b42ff9e8
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-10 13:23:00 -0400

    cleaned up config

--- monophoton/config.py
+++ monophoton/config.py
@@ -1,13 +1,16 @@
 import os
 
-# tier3 set up
-skimDir = '/mnt/hadoop/scratch/' + os.environ['USER'] + '/monophoton/skim'
-histDir = '/data/t3home000/' + os.environ['USER'] + '/studies/monophoton_panda'
+# location of source panda files
 ntuplesDir = '/mnt/hadoop/cms/store/user/paus'
 #ntuplesDir = '/mnt/hadoop/scratch/yiiyama'
-dataNtuplesDir = ntuplesDir
 
-#libobjs = os.environ['CMSSW_BASE'] + '/src/PandaTree/lib/libpanda.so'
+# location of skim output from ssw2
+skimDir = '/mnt/hadoop/scratch/' + os.environ['USER'] + '/monophoton/skim'
+# optionally copy to a local disk to speed up
+localSkimDir = '/local/' + os.environ['USER'] + '/monophoton/skim'
+
+# where the various output plots and text files
+histDir = '/data/t3home000/' + os.environ['USER'] + '/monophoton_noichmax'
+
+# panda library
 libobjs = 'libPandaTreeObjects.so'
-libutils = 'libPandaTreeUtils.so'
-dataformats = os.environ['CMSSW_BASE'] + '/src/PandaTree'
