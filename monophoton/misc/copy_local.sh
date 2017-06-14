7bdfedb57f9841ac919627caed7e4484e1ef8e00
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-14 09:57:55 -0400

    adding a utility script copy_local

---
+++ monophoton/misc/copy_local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CWD=$(pwd)
+
+cd $(dirname $(cd $(dirname $0); pwd))
+SKIMDIR=$(python -c "import config; print config.skimDir")
+LOCALDIR=$(python -c "import config; print config.localSkimDir")
+
+for FILE in $(rsync -nuv $SKIMDIR/*.root $LOCALDIR/ | grep .root)
+do
+  echo $FILE
+  rm $LOCALDIR/$FILE 2> /dev/null
+  hdfs dfs -fs hdfs://t3serv002.mit.edu:9000 -get $(echo $SKIMDIR | sed 's|/mnt/hadoop||')/$FILE $LOCALDIR/$FILE
+done
