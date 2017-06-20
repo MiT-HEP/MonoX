4713343b6f89eecfa64e9f81a67a5650f5213566
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-20 13:02:17 -0400

    fit updates

--- monophoton/fit/parameters.py
+++ monophoton/fit/parameters.py
@@ -1,6 +1,6 @@
 import os
 
-workdir = '/data/t3home000/' + os.environ['USER'] + '/monophoton_noichmax'
+workdir = '/data/t3home000/' + os.environ['USER'] + '/monophoton'
 distribution = 'phoPtHighMet'
 
 sourcedir = workdir + '/plots'
@@ -13,10 +13,11 @@ histname = distribution + '/{process}'
 signalHistname = distribution + '/samples/{process}_monoph'
 
 # sr = 'bmonoph' # blinded version
-sr = 'monoph'
+sr = 'monophout'
 
-regions = [sr, 'monoel', 'monomu', 'diel', 'dimu'] # , 'lowdphi'] # , 'lowmt']
-processes = ['data_obs', 'efake', 'gjets', 'halo', 'hfake', 'minor', 'spike', 'vvg', 'wg', 'zg', 'gg', 'wjets', 'top', 'zjets']
+regions = [sr, 'monophin', 'monoel', 'monomu', 'diel', 'dimu'] # , 'lowdphi'] # , 'lowmt']
+data = 'data_obs' # name of data process
+processes = ['efake', 'gjets', 'halo', 'hfake', 'minor', 'spike', 'vvg', 'wg', 'zg', 'gg', 'wjets', 'top', 'zjets']
 #signals = ['dmv-500-1', 'dmv-1000-1', 'dmv-2000-1', 'dma-500-1', 'dma-1000-1', 'dma-2000-1', 'dmvlo-500-1', 'dmvlo-1000-1', 'dmvlo-2000-1', 'dmalo-1000-1', 'dmalo-2000-1', 'dph-1000', 'dph-125']
 signals = ['dmvlo-1000-1']
 xtitle = 'p_{T}^{#gamma} (GeV)'
@@ -34,6 +35,9 @@ links = [
     (('zg', 'monoel'), ('zg', sr)),
     (('zg', 'monomu'), ('zg', sr)),
     (('wg', sr), ('zg', sr)),
+    (('zg', 'monophin'), ('zg', sr)),
+    (('wg', 'monophin'), ('wg', sr)),
+    (('halo', 'monophin'), ('halo', sr))
 #     (('gjets', 'lowdphi'), ('gjets', sr)),
 #     (('zg', 'lowdphi'), ('zg', sr)),
 #     (('wg', 'lowdphi'), ('wg', sr))
@@ -49,10 +53,12 @@ ignoredNuisances = {
     ('wg', 'monomu'): ['leptonVetoSF', 'vgPDF', 'vgQCDscale', 'EWK', 'gec'],
     ('zg', 'monoel'): ['leptonVetoSF', 'vgPDF', 'vgQCDscale', 'EWK', 'gec'],
     ('zg', 'monomu'): ['leptonVetoSF', 'vgPDF', 'vgQCDscale', 'EWK', 'gec'],
+    ('zg', 'monophin'): ['leptonVetoSF', 'vgPDF', 'vgQCDscale', 'EWK', 'gec'],
+    ('wg', 'monophin'): ['leptonVetoSF', 'vgPDF', 'vgQCDscale', 'EWK', 'gec'],
+    ('halo', 'monophin'): ['haloShape', 'haloNorm']
 #     ('zg', 'lowdphi'): ['leptonVetoSF', 'vgPDF', 'vgQCDscale', 'EWK', 'gec'],
 #     ('wg', 'lowdphi'): ['leptonVetoSF', 'vgPDF', 'vgQCDscale', 'EWK', 'gec'],
 #     ('halo', 'lowdphi'): ['haloShape'],
-    ('wg', sr): ['leptonVetoSF', 'gec']
 }
 
 # Artificial bin-to-bin decorrelation (de-shaping)
@@ -69,6 +75,7 @@ ratioCorrelations = {
 }
 
 # Nuisances affecting normalization only
-scaleNuisances = ['lumi', 'photonSF', 'customIDSF', 'leptonVetoSF', 'egFakerate', 'haloNorm', 'spikeNorm', 'minorQCDScale', 'muonSF', 'electronSF'] # lepton SF also flat for now
+#scaleNuisances = ['lumi', 'photonSF', 'customIDSF', 'leptonVetoSF', 'egFakerate', 'haloNorm', 'spikeNorm', 'minorQCDScale', 'muonSF', 'electronSF'] # lepton SF also flat for now
+scaleNuisances = ['lumi', 'photonSF', 'customIDSF', 'leptonVetoSF', 'egFakerate', 'spikeNorm', 'minorQCDScale', 'muonSF', 'electronSF'] # lepton SF also flat for now
 
 # def customize(workspace):
