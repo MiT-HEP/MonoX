1a7a27be528e5c8c6758c58c5490b1065e451fc3
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-20 13:06:54 -0400

    new plot-only mode

--- monophoton/trigger/eff.py
+++ monophoton/trigger/eff.py
@@ -15,25 +15,25 @@ sys.path.append(basedir)
 from datasets import allsamples
 from plotstyle import SimpleCanvas
 import config
+import utils
 
 ROOT.gStyle.SetNdivisions(510, 'X')
 ROOT.gSystem.Load('libPandaTreeObjects.so')
 ROOT.gROOT.LoadMacro(basedir + '/../common/MultiDraw.cc+')
 
+REPLOT = False
 FITEFFICIENCY = False
 
 measurements = [
-#    ('photon', 'sph', 'sph-16*', 'tpeg', 'probes.medium'),
-    ('photon', 'sel', 'sel-16*-m', 'tpeg', 'probes.medium && !probes.pixelVeto'),
-#    ('electron', 'sel', 'sel-16*', 'tpeg', 'probes.tight'),
-#    ('muon', 'smu', 'smu-16*', 'tpmg', 'probes.tight')
+    ('photon', 'sel', 'sel-16c-m', 'tpeg', 'probes.medium && !probes.pixelVeto'),
+    ('electron', 'sel', 'sel-16*-m', 'tp2e', 'probes.tight'),
+    ('muon', 'smu', 'smu-16*', 'tp2m', 'probes.tight')
 ]
 
 ## SETUP
 
 outName = 'trigger'
-outFileName = config.histDir + '/trigger/' + outName + '.root'
-outputFile = ROOT.TFile.Open(outFileName, 'recreate')
+outFileName = basedir + '/data/trigger_efficiency.root'
 
 vconfs = {}
 tconfs = {}
@@ -56,8 +56,6 @@ vconfs['electron'] = {
 vconfs['muon'] = {
     'pt': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', (50, 0., 50.)),
     'ptzoom': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', [30. + 2. * x for x in range(85)] + [200. + 10. * x for x in range(10)]),
-    'hOverE': ('H/E', 'probes.hOverE', 'probes.pt_ > 200.', (25, 0., 0.05)),
-    'hcalE': ('E^{HCAL} (GeV)', 'probes.pt_ * TMath::CosH(probes.eta_) * probes.hOverE', 'probes.pt_ > 200.', (25, 0., 5)),
     'run': ('Run', 'runNumber', 'probes.pt_ > 200.', (350, 271000., 274500.)),
     'leppt': ('p_{T}^{#gamma} (GeV)', 'probes.pt_', '', [0. + 5. * x for x in range(10)] + [50. + 10. * x for x in range(6)]),
 }
@@ -87,72 +85,85 @@ vtconfs['electron'] = [
 vtconfs['muon'] = []
 
 
-## FILL DISTRIBUTIONS AND GRAPHS
+if not REPLOT:
+    ## FILL DISTRIBUTIONS AND GRAPHS
 
-for oname, mname, snames, region, probeSel in measurements:
-    measDir = outputFile.mkdir(oname + '_' + mname)
+    outputFile = ROOT.TFile.Open(outFileName, 'recreate')
+    
+    for oname, mname, snames, region, probeSel in measurements:
+        print oname, mname
 
-    baseSelection = 'tp.mass > 60. && tp.mass < 120. && ' + probeSel
+        measDir = outputFile.mkdir(oname + '_' + mname)
+    
+        baseSelection = 'tp.mass > 60. && tp.mass < 120. && ' + probeSel
+    
+        # fill the histograms
+        plotter = ROOT.MultiDraw()
+        plotter.setWeightBranch('')
+    
+        for sample in allsamples.getmany(snames):
+            plotter.addInputPath(utils.getSkimPath(sample.name, region))
+    
+        plotter.setBaseSelection(baseSelection)
+    
+        # make empty histograms for all (variable, trigger) combination
+        histograms = []
+    
+        for vname, (vtitle, vexpr, baseline, binning) in vconfs[oname].items():
+            outDir = measDir.mkdir(vname)
+    
+            if type(binning) is tuple:
+                template = ROOT.TH1D('template', ';' + vtitle, *binning)
+            else:
+                template = ROOT.TH1D('template', ';' + vtitle, len(binning) - 1, array.array('d', binning))
+    
+            for tname, (passdef, denomdef, title) in tconfs[oname].items():
+                outDir.cd()
+                hpass = template.Clone(tname + '_pass')
+                hbase = template.Clone(tname + '_base')
+                histograms.extend([hpass, hbase])
+    
+                sels = [passdef]
+                if baseline:
+                    sels.append(baseline)
+                if denomdef:
+                    sels.append(denomdef)
+    
+                plotter.addPlot(hpass, vexpr, ' && '.join(sels), True)
+    
+                sels = []
+                if baseline:
+                    sels.append(baseline)
+                if denomdef:
+                    sels.append(denomdef)
+    
+                plotter.addPlot(hbase, vexpr, ' && '.join(sels), True)
+    
+            template.Delete()
+    
+        plotter.fillPlots()
+    
+        # make efficiency graphs and save
+        for vname in vconfs[oname]:
+            print ' ', vname
 
-    for vname, (vtitle, vexpr, baseline, binning) in vconfs[oname].items():
+            for tname in tconfs[oname]:
+                print '   ', tname
 
-    # fill the histograms
-    plotter = ROOT.MultiDraw()
-    plotter.setBaseSelection(baseSelection)
+                hpass = measDir.Get(vname + '/' + tname + '_pass')
+                hbase = measDir.Get(vname + '/' + tname + '_base')
+                eff = ROOT.TGraphAsymmErrors(hpass, hbase)
+                measDir.GetDirectory(vname).cd()
+                eff.Write(tname + '_eff')
+    
+        # save the distributions
+        for hist in histograms:
+            hist.GetDirectory().cd()
+            hist.Write()
 
-    for sample in allsamples.getmany(snames):
-        plotter.addInputPath(config.skimDir + '/' + sample.name + '_' + region + '.root')
+else:
+    outputFile = ROOT.TFile.Open(outFileName)
 
-    # make empty histograms for all (variable, trigger) combination
-    histograms = []
-
-    for vname, (vtitle, vexpr, baseline, binning) in vconfs[oname].items():
-        outDir = measDir.mkdir(vname)
-
-        if type(binning) is tuple:
-            template = ROOT.TH1D('template', ';' + vtitle, *binning)
-        else:
-            template = ROOT.TH1D('template', ';' + vtitle, len(binning) - 1, array.array('d', binning))
-
-        for tname, (passdef, denomdef, title) in tconfs[oname].items():
-            outDir.cd()
-            hpass = template.Clone(tname + '_pass')
-            hbase = template.Clone(tname + '_base')
-            histograms.extend([hpass, hbase])
-
-            sels = [passdef]
-            if baseline:
-                sels.append(baseline)
-            if denomdef:
-                sels.append(denomdef)
-
-            plotter.addPlot(hpass, vexpr, ' && '.join(sels), True)
-
-            sels = []
-            if baseline:
-                sels.append(baseline)
-            if denomdef:
-                sels.append(denomdef)
-
-            plotter.addPlot(hbase, vexpr, ' && '.join(sels), True)
-
-        template.Delete()
-
-    plotter.fillPlots()
-
-    # make efficiency graphs and save
-    for vname in vconfs[oname]:
-        for tname in tconfs[oname]:
-            hpass = measDir.Get(vname + '/' + tname + '_pass')
-            hbase = measDir.Get(vname + '/' + tname + '_base')
-            eff = ROOT.TGraphAsymmErrors(hpass, hbase)
-            measDir.GetDirectory(vname).cd()
-            eff.Write(tname + '_eff')
-
-    # save the distributions
-    for hist in histograms:
-        hist.GetDirectory().cd()
-        hist.Write()
 
 ## PLOT GRAPHS
 
@@ -168,14 +179,20 @@ canvas = SimpleCanvas()
 canvas.legend.setPosition(0.7, 0.3, 0.9, 0.5)
 
 for oname, mname, snames, region, probeSel in measurements:
+    print oname, mname
+
     measDir = outputFile.GetDirectory(oname + '_' + mname)
 
     canvas.lumi = sum(sample.lumi for sample in allsamples.getmany(snames))
 
     for vname, (vtitle, vexpr, baseline, binning) in vconfs[oname].items():
+        print ' ', vname
+
         outDir = measDir.GetDirectory(vname)
 
         for tname, (passdef, denomdef, title) in tconfs[oname].items():
+            print '   ', tname
+
             eff = outDir.Get(tname + '_eff')
 
             canvas.Clear()
