84eeec10c0230131b9ed2767c1583ae2883aeca4
Yutaro Iiyama <yiiyama@mit.edu>
2017-06-28 20:53:48 -0400

    new kfactors

--- monophoton/kfactor/kfactor.py
+++ monophoton/kfactor/kfactor.py
@@ -13,12 +13,14 @@ def makeZGWGHistograms(sname, binning):
     sample = allsamples[sname]
 
     tree = ROOT.TChain('events')
-    tree.Add(config.ntuplesDir + '/' + sample.fullname + '/simpletree_*.root')
+    for fullname in sample.datasetNames:
+        tree.Add(config.ntuplesDir + '/' + sample.book + '/' + fullname + '/*.root')
 
     lo = ROOT.TH1D('lo', '', len(binning) - 1, binning)
-    tree.Draw('promptFinalStates.pt>>lo', 'promptFinalStates.pid == 22 && TMath::Abs(promptFinalStates.eta) < 1.4442', 'goff')
 
-    lo.Scale(1000. * sample.crosssection / sample.sumw) # nnlo file given in fb
+    tree.Draw('partons.pt_>>lo', 'partons.pdgid == 22 && TMath::Abs(partons.eta_) < 1.4442', 'goff')
+
+    lo.Scale(1000. * sample.crosssection / sample.sumw, 'width') # nnlo file given in dsigma / dpT (fb / GeV)
 
     outFile.cd()
     kfactor = ROOT.TH1D(sname, '', len(binning) - 1, binning)
@@ -40,15 +42,13 @@ func.Write('gj-200')
 func.Write('gj-400')
 func.Write('gj-600')
 
-print 'znng & zllg'
-
 # the input file from Grazzini has a 1000- bin but has the same cross section as 700-1000.
 # guessing this means that the calculation was only up to 1000 GeV
 binning = array.array('d', [175., 190., 250., 400., 700., 1000.])
 
-lo, kfactor, kfactorUp, kfactorDown = makeZGWGHistograms('znng-130', binning)
+print 'znng & zllg'
 
-lo.Scale(1., 'width') # nnlo file is dsigma / dpT
+lo, kfactor, kfactorUp, kfactorDown = makeZGWGHistograms('znng-130-o', binning)
 
 with open(basedir + '/data/raw/znng_grazzini.dat') as source:
     source.readline()
@@ -58,6 +58,10 @@ with open(basedir + '/data/raw/znng_grazzini.dat') as source:
         kfactor.SetBinContent(iX, float(words[1]) * 3.) # three neutrino flavors
         kfactorDown.SetBinContent(iX, float(words[3]) * 3.)
         kfactorUp.SetBinContent(iX, float(words[5]) * 3.)
+
+        if iX == lo.GetNbinsX():
+            break
+
         iX += 1
 
 kfactor.Divide(lo)
@@ -70,30 +74,45 @@ kfactor.Write()
 kfactorUp.Write()
 kfactorDown.Write()
 
-kfactor.Write('zllg-130')
-kfactorUp.Write('zllg-130_scaleUp')
-kfactorDown.Write('zllg-130_scaleDown')
+kfactor.Write('zllg-130-o')
+kfactorUp.Write('zllg-130-o_scaleUp')
+kfactorDown.Write('zllg-130-o_scaleDown')
+
+kfactor.Write('zllg-300-o')
+kfactorUp.Write('zllg-300-o_scaleUp')
+kfactorDown.Write('zllg-300-o_scaleDown')
 
 print 'wnlg'
 
-binning = array.array('d', [175., 1000.])
+lo, kfactor, kfactorUp, kfactorDown = makeZGWGHistograms('wnlg-130-o', binning)
 
-lo, kfactor, kfactorUp, kfactorDown = makeZGWGHistograms('wnlg-130', binning)
-
-xsec = 0.
-xsecdown = 0.
-xsecup = 0.
-with open(basedir + '/data/raw/wnlg_grazzini.dat') as source:
+with open(basedir + '/data/raw/wmnlg_grazzini.dat') as source:
     source.readline()
+    iX = 1
     for line in source:
         words = line.split()
-        xsec += float(words[1]) * 3.
-        xsecdown += float(words[2]) * 3.
-        xsecup += float(words[3]) * 3.
+        kfactor.SetBinContent(iX, float(words[1]) * 3.)
+        kfactorDown.SetBinContent(iX, float(words[3]) * 3.)
+        kfactorUp.SetBinContent(iX, float(words[5]) * 3.)
 
-kfactor.SetBinContent(1, xsec)
-kfactorUp.SetBinContent(1, xsecup)
-kfactorDown.SetBinContent(1, xsecdown)
+        if iX == lo.GetNbinsX():
+            break
+
+        iX += 1
+
+with open(basedir + '/data/raw/wpnlg_grazzini.dat') as source:
+    source.readline()
+    iX = 1
+    for line in source:
+        words = line.split()
+        kfactor.SetBinContent(iX, kfactor.GetBinContent(iX) + float(words[1]) * 3.)
+        kfactorDown.SetBinContent(iX, kfactorDown.GetBinContent(iX) + float(words[3]) * 3.)
+        kfactorUp.SetBinContent(iX, kfactorUp.GetBinContent(iX) + float(words[5]) * 3.)
+
+        if iX == lo.GetNbinsX():
+            break
+
+        iX += 1
 
 kfactor.Divide(lo)
 kfactorUp.Divide(lo)
