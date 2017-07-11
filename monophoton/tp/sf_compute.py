9d54f4ff272295b8f5876390202e462831ad396f
Brandon <ballen@mit.edu>
2017-07-11 10:16:01 -0400

    Add MC truth result to scale factor calculation.

--- monophoton/tp/sf_compute.py
+++ monophoton/tp/sf_compute.py
@@ -27,7 +27,6 @@ if len(binning) == 0:
 
 sys.argv = []
 
-
 import ROOT
 ROOT.gROOT.SetBatch(True)
 ROOT.gSystem.Load('libRooFit.so')
@@ -42,14 +41,20 @@ outputFile = ROOT.TFile.Open(outputDir + '/scaleFactor_' + binningName + '.root'
 
 dataEff = dataSource.Get('eff')
 mcEff = mcSource.Get('eff')
+mcTruthEff = mcSource.Get('eff_truth')
 
 scaleFactor = dataEff.Clone('scaleFactor')
 scaleFactor.Divide(mcEff)
 
+sfTruth = dataEff.Clone('sf_truth')
+sfTruth.Divide(mcTruthEff)
+
 outputFile.cd()
 scaleFactor.Write()
+sfTruth.Write()
 dataEff.Write('dataEff')
 mcEff.Write('mcEff')
+mcTruthEff.Write('mcTruthEff')
 
 ### Visualize
 
@@ -62,15 +67,24 @@ canvas.SetGrid(False, True)
 canvas.legend.setPosition(0.7, 0.8, 0.9, 0.9)
 
 canvas.legend.add('sf', 'Scale Factor', opt = 'LP', color = ROOT.kBlack, mstyle = 8)
+canvas.legend.add('sf_truth', 'MC truth', opt = 'LP', color = ROOT.kGreen, mstyle = 4)
 canvas.ylimits = (0.8, 1.20)
 
 canvas.legend.apply('sf', scaleFactor)
 canvas.addHistogram(scaleFactor, drawOpt = 'EP')
 
+canvas.legend.apply('sf_truth', sfTruth)
+canvas.addHistogram(sfTruth, drawOpt = 'EP')
+
 canvas.xtitle = binningTitle
 canvas.printWeb('efake', 'scaleFactor_' + binningName, logy = False)
 
+print 'Fit Results:'
 for iBin, (bin, _) in enumerate(fitBins):
     print '%15s [%.3f +- %.3f]' % (bin, scaleFactor.GetBinContent(iBin + 1), scaleFactor.GetBinError(iBin + 1))
+
+print '\nTruth Results:'
+for iBin, (bin, _) in enumerate(fitBins):
+    print '%15s [%.3f +- %.3f]' % (bin, sfTruth.GetBinContent(iBin + 1), sfTruth.GetBinError(iBin + 1))
 
 outputFile.Close()
