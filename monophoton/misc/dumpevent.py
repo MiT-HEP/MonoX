6a967d59ca06b76b3c291a2cca04b56117a4051a
Yutaro Iiyama <yiiyama@mit.edu>
2017-07-14 04:36:10 -0400

    simple utility to make an event dump

---
+++ monophoton/misc/dumpevent.py
@@ -0,0 +1,15 @@
+import sys
+
+import ROOT
+ROOT.gSystem.Load('libPandaTreeObjects.so')
+## need to instantiate ROOT.panda (otherwise CLING segfaults)
+#e = ROOT.panda.Event
+
+if len(sys.argv) > 2:
+    run, lumi, event = map(int, sys.argv[2].split(':'))
+else:
+    run, lumi, event = 0, 0, 0
+
+ROOT.gROOT.LoadMacro('dumpevent.C')
+
+ROOT.dumpevent(sys.argv[1], run, lumi, event)
