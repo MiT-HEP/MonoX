33e10736567a53dc0816b9c1b1bb6af7baa050d5
Brandon <ballen@mit.edu>
2017-09-18 10:28:22 -0400

    use parallel option when make impact plots.

--- monophoton/fit/impacts.sh
+++ monophoton/fit/impacts.sh
@@ -6,7 +6,7 @@ datacard=$3
 
 text2workspace.py ${datacard} -m 125 -o ${outname}.root
 combineTool.py -M Impacts -d ${outname}.root -m 125 --robustFit 1 --freezeNuisances lumiscale --setPhysicsModelParameterRanges r=-5.0,5.0 --expectSignal=${signal} -t -1 --doInitialFit
-combineTool.py -M Impacts -d ${outname}.root -m 125 --robustFit 1 --freezeNuisances lumiscale --setPhysicsModelParameterRanges r=-5.0,5.0 --expectSignal=${signal} -t -1 --doFits
+combineTool.py -M Impacts -d ${outname}.root -m 125 --robustFit 1 --freezeNuisances lumiscale --setPhysicsModelParameterRanges r=-5.0,5.0 --expectSignal=${signal} -t -1 --doFits --parallel 24
 combineTool.py -M Impacts -d ${outname}.root -m 125 -o impacts.json_${outname}
 plotImpacts.py -i impacts.json_${outname} -o impacts_${outname}
 
