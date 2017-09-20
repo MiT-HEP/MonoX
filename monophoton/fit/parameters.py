6db257d995af04d3f777d684763a0fc39f87378a
Brandon <ballen@mit.edu>
2017-09-20 12:54:37 -0400

    float all bins of halo with a single free parameter instead of one per bin. add halo shape uncertainty.

--- monophoton/fit/parameters.py
+++ monophoton/fit/parameters.py
@@ -18,7 +18,7 @@ sr = 'monophHighPhi'
 
 regions = [sr, 'monophLowPhi', 'monoel', 'monomu', 'diel', 'dimu'] # + ['lowdphi'] # , 'lowmt']
 data = 'data_obs' # name of data process
-processes = ['efake', 'gjets', 'hfake', 'minor', 'vvg', 'wg', 'zg', 'gg', 'wjets', 'top', 'zjets' ] +  ['halo', 'spike', ]
+processes = ['efake', 'gjets', 'hfake', 'minor', 'vvg', 'wg', 'zg', 'gg', 'wjets', 'top', 'zjets', 'spike'] + ['halo']
 #signals = ['dmv-500-1', 'dmv-1000-1', 'dmv-2000-1', 'dma-500-1', 'dma-1000-1', 'dma-2000-1', 'dmvlo-500-1', 'dmvlo-1000-1', 'dmvlo-2000-1', 'dmalo-1000-1', 'dmalo-2000-1', 'dph-1000', 'dph-125']
 signals = ['dmvlo-1000-1'] # 
 xtitle = 'p_{T}^{#gamma} (GeV)'
@@ -60,10 +60,10 @@ ignoredNuisances = {
     ('zg', 'monomu'): wzIgnoreListExp + wzIgnoreListThe,
     ('zg', 'monophLowPhi'): wzIgnoreListExp + wzIgnoreListThe,
     ('wg', 'monophLowPhi'): wzIgnoreListExp + wzIgnoreListThe,
-    ('halo', 'monophLowPhi'): ['haloShape', 'haloNorm'],
+    ('halo', 'monophLowPhi'): ['haloNorm'],
     ('zg', 'monophHighPhi'): wzIgnoreListExp,
     ('wg', 'monophHighPhi'): wzIgnoreListExp,
-    ('halo', 'monophHighPhi'): ['haloShape', 'haloNorm'],
+    ('halo', 'monophHighPhi'): ['haloNorm'],
 #    ('zg', 'lowdphi'): wzIgnoreListExp + wzIgnoreListThe,
 #    ('wg', 'lowdphi'): wzIgnoreListExp + wzIgnoreListThe,
 #    ('gjets', 'lowdphi'): gjIgnoreList,
