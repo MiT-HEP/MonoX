9f340def9e5791f18ba5e4ba24d557a497c669df
Yutaro Iiyama <yiiyama@mit.edu>
2018-02-20 16:55:28 -0500

    more asimov tweaks

--- monophoton/main/plot.py
+++ monophoton/main/plot.py
@@ -628,7 +628,7 @@ if __name__ == '__main__':
         for plotdef in plotdefs:
             plotdef.blind = None
 
-    if args.blind or args.asimov:
+    if args.blind:
         for plotdef in plotdefs:
             plotdef.blind = 'full'
 
@@ -689,7 +689,9 @@ if __name__ == '__main__':
     
                 sspec.group.samples.append(sspec.sample)
 
-        groups.append(plotConfig.obs)
+        if not args.asimov:
+            # if args.asimov, we'll make the data_obs plot below
+            groups.append(plotConfig.obs)
     
         for group in groups:
             print ' ', group.name
@@ -711,6 +713,8 @@ if __name__ == '__main__':
             writeHist(bkghistSyst)
 
             if args.asimov:
+                asimov = bkghist.Clone('asimov')
+
                 # generate the "observed" distribution from background total
                 for varspec in args.asimov_variation:
                     # example: fakemet:fakemetShapeUp:5
@@ -731,23 +735,20 @@ if __name__ == '__main__':
                         print 'Invalid variation specified for pseudo-data:', varspec
                         continue
 
-                    bkghist.Add(nominal, -1.)
-                    bkghist.Add(varhist, scale)
-
-                # data_obs is empty at this point because blind is 'full'
-                obshist = outDir.Get('data_obs')
-
-                for iBin in xrange(1, bkghist.GetNbinsX() + 1):
-                    x = bkghist.GetXaxis().GetBinCenter(iBin)
-                    for _ in xrange(int(round(bkghist.GetBinContent(iBin)))):
-                        obshist.Fill(x)
+                    asimov.Add(nominal, -1.)
+                    asimov.Add(varhist, scale)
 
                 if args.asimov != 'background':
                     sighist = outDir.Get('samples/' + args.asimov + '_' + plotConfig.name)
-                    for iBin in xrange(1, sighist.GetNbinsX() + 1):
-                        x = sighist.GetXaxis().GetBinCenter(iBin)
-                        for _ in xrange(int(round(sighist.GetBinContent(iBin)))):
-                            obshist.Fill(x)
+                    asimov.Add(sighist)
+
+                # make data_obs here
+                obshist = plotdef.makeHist('data_obs', outDir = outDir)
+
+                for iBin in xrange(1, asimov.GetNbinsX() + 1):
+                    x = asimov.GetXaxis().GetBinCenter(iBin)
+                    for _ in xrange(int(round(asimov.GetBinContent(iBin)))):
+                        obshist.Fill(x)
 
                 writeHist(obshist)
 
