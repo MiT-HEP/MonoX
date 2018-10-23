246aa47df31def49d96f50e4efce6527ebb363f9
Yutaro Iiyama <yiiyama@mit.edu>
2018-10-23 13:17:40 -0400

    fixed lumi

--- monophoton/configs/Monophoton2017/params.py
+++ monophoton/configs/Monophoton2017/params.py
@@ -3,7 +3,7 @@ import os
 _localdir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(os.path.dirname(_localdir))
 
-lumilist = basedir + '/data/lumis16_plain.txt'
-datasetlist = basedir + '/data/datasets16.csv'
+lumilist = basedir + '/data/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt'
+datasetlist = basedir + '/data/datasets17.csv'
 
-pureweight = ('puweight_fulllumi', basedir + '/data/pileup16.root')
+pureweight = basedir + '/data/pileup17.root'
--- monophoton/configs/TagAndProbe2017/params.py
+++ monophoton/configs/TagAndProbe2017/params.py
@@ -3,7 +3,7 @@ import os
 _localdir = os.path.dirname(os.path.realpath(__file__))
 basedir = os.path.dirname(os.path.dirname(_localdir))
 
-lumilist = basedir + '/data/lumis17_plain.txt'
+lumilist = basedir + '/data/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt'
 datasetlist = basedir + '/data/datasets17.csv'
 
 pureweight = basedir + '/data/pileup17.root'
