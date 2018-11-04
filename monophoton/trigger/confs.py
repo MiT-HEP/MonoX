abf216f4ec249b610b7f5e0d7611a341c8264daa
Yutaro Iiyama <yiiyama@mit.edu>
2018-11-04 10:20:23 -0500

    update 2017

--- monophoton/trigger/confs.py
+++ monophoton/trigger/confs.py
@@ -89,7 +89,7 @@ if config.config == 'TagAndProbe2016':
 elif config.config == 'TagAndProbe2017':
     measurements = {
         ('photon', 'sel'): ('sel-17*', 'tpegLowPt', '(tags.hltmatchele27 || tags.hltmatchele32 || tags.hltmatchele35) && probes.medium && !probes.pixelVeto && tp.mass > 60. && tp.mass < 120. && TMath::Abs(TVector2::Phi_mpi_pi(probes.phi_ - tags.phi_)) > 0.6', 'probes'),
-        ('photon', 'dy'): (['dy-50@', 'dy-50-*'], 'tpegLowPt', 'probes.medium && !probes.pixelVeto && tp.mass > 60. && tp.mass < 120. && TMath::Abs(TVector2::Phi_mpi_pi(probes.phi_ - tags.phi_)) > 0.6', 'probes'),
+        ('photon', 'dy'): (['dy-50'], 'tpegLowPt', '(tags.hltmatchele27 || tags.hltmatchele32 || tags.hltmatchele35) && probes.medium && !probes.pixelVeto && tp.mass > 60. && tp.mass < 120. && TMath::Abs(TVector2::Phi_mpi_pi(probes.phi_ - tags.phi_)) > 0.6', 'probes'),
         ('photon', 'elmu'): (['smu-17*'], 'elmu', 'photons.mediumX[][2]', 'photons'),
         ('photon', 'ph75'): (['sph-17b', 'sph-17c', 'sph-17d'], 'ph75', 'photons.medium && HLT_Photon50 && runNumber < 276525', 'photons'),
         ('photon', 'ph75h'): (['sph-17b', 'sph-17c', 'sph-17d'], 'ph75', 'photons.medium && HLT_Photon75 && runNumber < 276525', 'photons'),
@@ -107,22 +107,22 @@ elif config.config == 'TagAndProbe2017':
             'l1eg': ('{col}.hltmatchphoton200L1Seed', '', 'L1 seed', {
                 'pt': ('p_{T}^{#gamma} (GeV)', '{col}.pt_', '', (50, 0., 100.)),
                 'ptwide': ('p_{T}^{#gamma} (GeV)', '{col}.pt_', '', [30. + 5. * x for x in range(14)] + [100. + 10. * x for x in range(10)] + [200. + 20. * x for x in range(5)] + [300., 350., 400.]),
-                'hOverE': ('H/E', '{col}.hOverE', '{col}.pt_ > 175.', (25, 0., 0.05)),
-                'hcalE': ('E^{HCAL} (GeV)', '{col}.pt_ * TMath::CosH({col}.eta_) * {col}.hOverE', '{col}.pt_ > 175.', (25, 0., 5)),
-                'run': ('Run', 'runNumber', '{col}.pt_ > 175.', (26, 294927, 306462))
+                'hOverE': ('H/E', '{col}.hOverE', '{col}.pt_ > 210.', (25, 0., 0.05)),
+                'hcalE': ('E^{HCAL} (GeV)', '{col}.pt_ * TMath::CosH({col}.eta_) * {col}.hOverE', '{col}.pt_ > 210.', (25, 0., 5)),
+                'run': ('Run', 'runNumber', '{col}.pt_ > 210.', (26, 294927, 306462))
             }),
             'sph200abs': ('{col}.hltmatchphoton200', '', 'L1&HLT', {
                 'pt': ('p_{T}^{#gamma} (GeV)', '{col}.pt_', '', [30. + 5. * x for x in range(14)] + [100. + 10. * x for x in range(10)] + [200. + 20. * x for x in range(5)] + [300. + 50. * x for x in range(10)]),
                 'ptzoom': ('p_{T}^{#gamma} (GeV)', '{col}.pt_', '', [30. + 5. * x for x in range(34)] + [200. + 15. * x for x in range(11)]),
-                'hOverE': ('H/E', '{col}.hOverE', '{col}.pt_ > 175.', (25, 0., 0.05)),
-                'hcalE': ('E^{HCAL} (GeV)', '{col}.pt_ * TMath::CosH({col}.eta_) * {col}.hOverE', '{col}.pt_ > 175.', (25, 0., 5)),
-                'run': ('Run', 'runNumber', '{col}.pt_ > 175.', (26, 294927, 306462))
+                'hOverE': ('H/E', '{col}.hOverE', '{col}.pt_ > 210.', (25, 0., 0.05)),
+                'hcalE': ('E^{HCAL} (GeV)', '{col}.pt_ * TMath::CosH({col}.eta_) * {col}.hOverE', '{col}.pt_ > 210.', (25, 0., 5)),
+                'run': ('Run', 'runNumber', '{col}.pt_ > 210.', (26, 294927, 306462))
             }),
             'ph75r9iso': ('{col}.hltmatchphoton75', '{col}.isEB', 'Photon75Iso40R9', {
                 'pt': ('p_{T}^{#gamma} (GeV)', '{col}.pt_', '{col}.r9 > 0.9', (50, 0., 100.)),
                 'ptwide': ('p_{T}^{#gamma} (GeV)', '{col}.pt_', '{col}.r9 > 0.9', [30. + 10. * x for x in range(7)] + [100., 120., 140., 160., 200., 300., 400., 600.]),
                 'r9': ('R^{9}', '{col}.r9', '{col}.pt_ > 80.', (30, 0.7, 1.)),
-                'run': ('Run', 'runNumber', '{col}.pt_ > 175.', (26, 294927, 306462))
+                'run': ('Run', 'runNumber', '{col}.pt_ > 210.', (26, 294927, 306462))
             })
         },
 #        'electron': {
