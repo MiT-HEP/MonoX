e6cb01e6c7ff8c6fbaff509c52b7f2103d7c5e1a
Yutaro Iiyama <yiiyama@mit.edu>
2018-11-04 10:19:38 -0500

    if test just echo

--- monophoton/misc/copy_local.sh
+++ monophoton/misc/copy_local.sh
@@ -11,8 +11,11 @@ LOCALDIR=$(python -c "import config; print config.localSkimDir")
 for FILE in $(rsync -nuv $SKIMDIR/*.root $LOCALDIR/ | grep .root)
 do
   echo $FILE
-  if ! [ $TEST ]
+  if [ $TEST ]
   then
+    echo rm $LOCALDIR/$FILE
+    echo hdfs dfs -fs hdfs://t3serv002.mit.edu:9000 -get $(echo $SKIMDIR | sed 's|/mnt/hadoop||')/$FILE $LOCALDIR/$FILE
+  else
     rm $LOCALDIR/$FILE 2> /dev/null
     hdfs dfs -fs hdfs://t3serv002.mit.edu:9000 -get $(echo $SKIMDIR | sed 's|/mnt/hadoop||')/$FILE $LOCALDIR/$FILE
   fi
