ab9045bca742ef93a875ce22e62821f4fbe95efe
Yutaro Iiyama <yiiyama@mit.edu>
2018-11-11 06:08:41 -0500

    common block

---
+++ monophoton/configs/common/selconf.py
@@ -0,0 +1,13 @@
+selconf = {
+    'photonFullSelection': [],
+    'photonIDTune': -1,
+    'photonWP': 1,
+    'ecalNoiseMap': [],
+    'photonSF': None,
+    'hadronTFactorSource': None,
+    'electronTFactor': '',
+    'electronTFactorUnc': 'frate',
+    'hadronProxyDef': ['!CHIso', '+CHIso11'],
+    'ewkCorrSource': '',
+    'sphTrigger': ''
+}
