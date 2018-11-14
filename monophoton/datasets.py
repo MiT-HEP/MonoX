dba801eafdd34c36e7227849fc87183f6fc21756
Yutaro Iiyama <yiiyama@mit.edu>
2018-11-14 09:09:57 -0500

    column width

--- monophoton/datasets.py
+++ monophoton/datasets.py
@@ -185,7 +185,7 @@ class SampleDef(object):
             comments = ''
             
         lineTuple = (self.name, title, xsecstr, self.nevents, sumwstr, self.generator, self.catalog[0], self.book, fullnames, comments)
-        return '%-16s %-35s %-16s %-10d %-20s %-12s %-9s %-12s %s%s' % lineTuple
+        return '%-16s %-35s %-16s %-10d %-20s %-9s %-8s %-12s %s%s' % lineTuple
 
     def _updateSumw(self):
         if self._sumw2 > 0.:
